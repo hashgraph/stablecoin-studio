27782378ef24feda3ca5ab06da9b335c3b6f148f
Adri <adrian@io.builders>
2022-08-26 12:16:18 +0200

    token creation
    Signed-off-by: sonia <sonia@io.builders>

--- contracts/scripts/deploySDK.ts
+++ contracts/scripts/deploySDK.ts
@@ -2,7 +2,7 @@
 import {deployContracts} from "./utilsToSDK";
 
 async function  main () {
-  deployContracts("TOKEN","TK",2,0,100000,"mytoken",false);
+  deployContracts("TOKEN","TK",2,0,100_000,"mytoken",false);
 }
 
 main();
