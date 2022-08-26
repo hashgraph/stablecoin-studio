49befc010d012fc4994e924f9435db961aa1a86a
Sergio Carmona <sergio.carmona@io.builders>
2022-08-26 12:42:08 +0200

    Migration with functionalities from IOB repo
    Signed-off-by: sonia <sonia@io.builders>

--- cli/src/domain/stablecoin/StableCoin.ts
+++ cli/src/domain/stablecoin/StableCoin.ts
@@ -1,5 +1,15 @@
 export interface StableCoin {
-    name:       string;
-    symbol:     string;
-    decimals:   number;
+  name: string;
+  symbol: string;
+  decimals: number;
+  initialSupply?: number;
+  supplyType?: string;
+  totalSupply?: number;
+  expirationTime?: number;
+  memo?: string;
+  freeze?: boolean;
+  KYC?: string;
+  wipe?: string;
+  feeSchedule?: string;
+  admin?: string;
 }
