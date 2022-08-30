18432e6edc7fd7978440d7b42ce5b6cdd9cc13f5
mamorales <miguelangel@io.builders>
2022-08-30 10:18:53 +0200

    Mint functionality
    Signed-off-by: mamorales <miguelangel@io.builders>

---
+++ contracts/test/token.cash-in.ts
@@ -0,0 +1,31 @@
+import { ContractFunctionParameters, ContractId, AccountId } from "@hashgraph/sdk";
+require("@hashgraph/hardhat-hethers");
+require("@hashgraph/sdk");
+
+import { expect } from "chai";
+import { deployContractsWithSDK, contractCall, getClient, createECDSAAccount } from "../scripts/utils";
+import { HederaERC20__factory } from "../typechain-types";
+
+import dotenv from "dotenv";
+
+describe("General ERC20", function() {
+  let proxyAddress:any;
+  let client:any;
+  let contractProxy: { name: (arg0: { gasLimit: number; }) => any; symbol: (arg0: { gasLimit: number; }) => any; decimals: (arg0: { gasLimit: number; }) => any; };
+
+  before(async function  () {
+    client = getClient(process.env.OPERATOR_ID!, process.env.OPERATOR_PRIVATE_KEY!);      
+  });
+  beforeEach(async function () {
+    proxyAddress = await deployContractsWithSDK("MIDAS", "MD", 3, 0, 1000, "Token para dejar de trabajar");    
+  });
+  it("Basic init params check", async function() {
+    const result = await contractCall(ContractId.fromString(proxyAddress), 'name', [], client, 60000, HederaERC20__factory.abi);
+    expect(result[0]).to.equals("MIDAS");
+
+    /*const account = await createECDSAAccount(client, 0);
+    const parametersContractCall = [AccountId.fromString(account.accountId).toSolidityAddress(), 1000];  
+    const result = await contractCall(ContractId.fromString(proxyAddress), 'mint2', parametersContractCall, client, 400000, HederaERC20__factory.abi)  
+    expect(result[0]).to.equals(true);*/
+  });
+});
