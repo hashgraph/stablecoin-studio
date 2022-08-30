f8e67c4e5bf0ec22b84f699ffdfeb8d5011d151a
mamorales <miguelangel@io.builders>
2022-08-30 18:15:20 +0200

    Modularization structure
    Signed-off-by: mamorales <miguelangel@io.builders>
    Signed-off-by: sonia <sonia@io.builders>

---
+++ contracts/contracts/TokenOwner.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./ITokenOwner.sol";
+
+abstract contract TokenOwner is ITokenOwner {
+    
+    HTSTokenOwner HTSTokenOwnerAddress;
+    address tokenAddress; 
+
+    function setTokenAddress(HTSTokenOwner _htsTokenOwnerAddress, address _tokenAddress) 
+        external         
+    {
+        require(tokenAddress == address(0), "Token address already defined");
+
+        HTSTokenOwnerAddress = _htsTokenOwnerAddress;
+        tokenAddress = _tokenAddress;
+    }
+
+    function getTokenAddress()  
+        public
+        view 
+        returns (address) 
+    {
+        return _getTokenAddress();
+    }
+
+    function _getTokenAddress()  
+        internal
+        view 
+        returns (address) 
+    {
+        return tokenAddress;
+    }
+
+    function getTokenOwnerAddress()
+        public
+        view 
+        returns (address) 
+    {
+        return _getTokenOwnerAddress();
+    }
+
+    function _getTokenOwnerAddress()
+        internal
+        view 
+        returns (address) 
+    {
+        return address(HTSTokenOwnerAddress);
+    }
+
+}
