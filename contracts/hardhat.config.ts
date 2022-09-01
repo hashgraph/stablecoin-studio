b20a4ebed68c2847b95fee6483cc217255a49dc8
mamorales <miguelangel@io.builders>
2022-09-01 13:36:49 +0200

    mend
    Signed-off-by: mamorales <miguelangel@io.builders>
    Signed-off-by: sonia <sonia@io.builders>

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -6,7 +6,7 @@ require('hardhat-abi-exporter');
 
 module.exports = {
   solidity: "0.8.10",
-  defaultNetwork: "localHederaNetwork", // The selected default network. It has to match the name of one of the configured networks.
+  defaultNetwork: "testnet", // The selected default network. It has to match the name of one of the configured networks.
   hedera: {
     gasLimit: 300000, // Default gas limit. It is added to every contract transaction, but can be overwritten if required.
     networks: {
@@ -21,28 +21,6 @@ module.exports = {
             publicKey:"302a300506032b657003210032c231261223d8667d841d7ca58abd9d0701eb03238a8ee4e5cdfba6925c3109",
             isED25519Type: true
           },
-        ],
-      },
-      localHederaNetwork: {
-        consensusNodes: [
-          {
-            url: "127.0.0.1:50211",
-            nodeId: "0.0.3",
-          },
-        ],
-        mirrorNodeUrl: "http://127.0.0.1:5551",
-        chainId: 0,
-        accounts: [
-         {
-           account: "0.0.2",
-            privateKey:
-             "302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137",
-          },
-          // {
-          //   account: "0.0.1002",
-          //   privateKey:
-          //     "0x7f109a9e3b0d8ecfba9cc23a3614433ce0fa7ddcc80f2a8f10b222179a5a80d6",
-          // },
         ],
       },
     },
