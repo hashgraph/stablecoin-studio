6413cf3c1ab2a143ca86b7961132fc747be888b3
Adri <adrian@io.builders>
2022-09-05 15:43:26 +0200

    remove old transfer
    Signed-off-by: sonia <sonia@io.builders>

--- contracts/contracts/HTSTokenOwner.sol
+++ contracts/contracts/HTSTokenOwner.sol
@@ -56,16 +56,6 @@ contract HTSTokenOwner is IHTSTokenOwner, HederaTokenService {
         return _checkResponse(transferResponse);
     }
 
-    function transfer(address tokenAddress,address to, uint256 amount) 
-        external 
-        onlyHederaERC20() 
-        returns (bool) 
-    {
-        address tokenAddress = tokenAddress;
-        int256 transferResponse = HederaTokenService.transferToken(tokenAddress, address(this), to, int64(int256(amount)));
-        return _checkResponse(transferResponse);
-    }
-
     function wipeToken(address tokenAddress, address account, uint32 amount) 
         external 
         onlyHederaERC20() 
