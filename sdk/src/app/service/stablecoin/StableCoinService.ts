992997125cff2a6ce29ef66aca79ce6c074578ee
sergio.alba1 <sergio.alba@io.builders>
2022-09-07 13:31:30 +0200

    Small refactor

--- sdk/src/app/service/stablecoin/StableCoinService.ts
+++ sdk/src/app/service/stablecoin/StableCoinService.ts
@@ -49,7 +49,6 @@ export default class StableCoinService extends Service {
 			req.supplyType,
 			req.id,
 		);
-		console.debug(coin);
 		return this.repository.saveCoin(req.accountId, req.privateKey, coin);
 	}
 
