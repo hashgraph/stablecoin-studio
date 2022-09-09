052e2d77f061bc9a216b7bc2c90da775ed721617
Manu Fernandez <manuel@io.builders>
2022-09-09 08:22:44 +0200

    Fix align center

--- web/src/layout/topbar/TopbarRight.tsx
+++ web/src/layout/topbar/TopbarRight.tsx
@@ -15,7 +15,7 @@ const TopbarRight = () => {
 				<Text>{network}</Text>
 			</Flex>
 			<Box borderLeft='2px solid #ECEBF1' w='1px' />
-			<Flex color='brand.gray' fontSize='12px' fontWeight='400'>
+			<Flex color='brand.gray' fontSize='12px' fontWeight='400' alignItems='center'>
 				<Text>{t('topbar.account')}</Text>
 				<Text mr='5px'>: </Text>
 				<Text>{accountId}</Text>
