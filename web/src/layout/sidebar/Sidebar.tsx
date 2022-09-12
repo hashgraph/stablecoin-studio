b12f261853e4abef6aff7fea1442bbd89601c6dc
Manu Fernandez <manuel@io.builders>
2022-09-12 11:04:54 +0200

    Fixed translations and sidebar width

--- web/src/layout/sidebar/Sidebar.tsx
+++ web/src/layout/sidebar/Sidebar.tsx
@@ -1,28 +1,31 @@
 import { Box, Flex } from '@chakra-ui/react';
+import { useTranslation } from 'react-i18next';
 import { NamedRoutes } from '../../Router/NamedRoutes';
 import SidebarOption from './SidebarOption';
 
 const Sidebar = () => {
+	const { t } = useTranslation('global');
+
 	const options = [
 		{
 			icon: 'Coin',
-			title: 'Operations',
+			title: t('sidebar.operations'),
 			route: NamedRoutes.Operations,
 		},
 		{
 			icon: 'Users',
-			title: 'Role management',
+			title: t('sidebar.role'),
 			route: NamedRoutes.Roles,
 		},
 		{
 			icon: 'Gear',
-			title: 'Stable coin details',
+			title: t('sidebar.details'),
 			route: NamedRoutes.Details,
 		},
 	];
 
 	return (
-		<Box bgColor='brand.white' minW='208px' p='32px 12px'>
+		<Box bgColor='brand.white' minW='240px' p='32px 12px'>
 			<Flex flexDirection='column' alignItems='center' gap={3}>
 				{options.map((option) => {
 					const { title } = option;
