44b2ed4b3d22a6b4d05ac0156c227fd4bb096a43
Manu Fernandez <manuel@io.builders>
2022-09-12 16:05:45 +0200

    Added DirectAccess Component

---
+++ web/src/components/DirectAccess.tsx
@@ -0,0 +1,54 @@
+import { Flex, Text, FlexProps } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { NamedRoutes } from '../Router/NamedRoutes';
+import { RouterManager } from '../Router/RouterManager';
+import Icon from './Icon';
+
+interface DirectAccessProps extends FlexProps {
+	title: string;
+	icon: string;
+	route: NamedRoutes;
+}
+
+const DirectAccess = ({ title, icon, route, ...props }: DirectAccessProps) => {
+	const navigate = useNavigate();
+
+	const handleNavigate = () => {
+		RouterManager.to(navigate, route);
+	};
+
+	return (
+		<Flex
+			w='120px'
+			h='136px'
+			borderRadius='16px'
+			gap='16px'
+			bgColor='brand.white'
+			flexDirection='column'
+			justifyContent='center'
+			alignItems='center'
+			onClick={handleNavigate}
+			_hover={{
+				cursor: 'pointer',
+				transform: 'scale(1.03)',
+			}}
+			{...props}
+		>
+			<Flex
+				h='48px'
+				w='48px'
+				borderRadius='50%'
+				bgColor='light.purple4'
+				justifyContent='center'
+				alignItems='center'
+			>
+				<Icon name={icon} fontSize='28px' color='light.purple3' />
+			</Flex>
+			<Text fontSize='14px' fontWeight='700' lineHeight='16px'>
+				{title}
+			</Text>
+		</Flex>
+	);
+};
+
+export default DirectAccess;
