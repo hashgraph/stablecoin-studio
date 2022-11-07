292f2bdf7912314bca4a42e1b8bfff9711e877c9
alberto <alberto@io.builders>
2022-11-07 14:26:43 +0100

    CLI updated with Wipe Request changes

--- cli/src/app/service/stablecoin/OperationStableCoinService.ts
+++ cli/src/app/service/stablecoin/OperationStableCoinService.ts
@@ -273,7 +273,6 @@ export default class OperationStableCoinService extends Service {
         try {
           await new WipeStableCoinsService().wipeStableCoin(
             this.proxyContractId,
-            currentAccount,
             this.stableCoinId,
             account2Wipe,
             amount2Wipe,
