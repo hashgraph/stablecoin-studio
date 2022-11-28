0a10eb37c8dcf7c7f9adb0f5c0b0335cd33007ed
Sergio Alba <sergio.alba@io.builders>
2022-11-28 12:08:04 +0100

    First attempt
    Signed-off-by: Sergio Alba <sergio.alba@io.builders>

---
+++ sdk/src/core/Type.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Type<T = any> extends Function {
+	new (...args: any[]): T;
+}
---
+++ sdk/src/core/decorator/CommandHandlerDecorator.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { COMMAND_HANDLER_METADATA, COMMAND_METADATA } from '../Constants';
+import { v4 } from 'uuid';
+import { Command } from '../command/Command.js';
+
+/**
+ * This decorator determines that a class is a command handler
+ *
+ * The decorated class must implement the `CommandHandler` interface.
+ *
+ * @param command command *type* to be handled by this handler.
+ */
+export const CommandHandler = (command: Command): ClassDecorator => {
+	return (target: object) => {
+		if (!Reflect.hasMetadata(COMMAND_METADATA, command)) {
+			Reflect.defineMetadata(COMMAND_METADATA, { id: v4() }, command);
+		}
+		Reflect.defineMetadata(COMMAND_HANDLER_METADATA, command, target);
+	};
+};
