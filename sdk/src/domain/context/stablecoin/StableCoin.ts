13e049f63120a5b7df17bf9af21c54682d134615
Axel <axel@io.builders>
2022-11-29 16:36:22 +0100

    add properties StableCoin
    Signed-off-by: Axel <axel@io.builders>

--- sdk/src/domain/context/stablecoin/StableCoin.ts
+++ sdk/src/domain/context/stablecoin/StableCoin.ts
@@ -2,7 +2,9 @@ import Contract from '../contract/Contract.js';
 
 export default class StableCoin {
 	constructor(
-        public proxy: Contract,
-        // ... rest
-    ) {}
+		public proxy: Contract,
+		public evmProxyAddress: string,
+		public tokenId: string,
+	) // ... rest
+	{}
 }
