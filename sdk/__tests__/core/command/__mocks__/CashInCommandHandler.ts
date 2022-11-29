ca166cedcd8ad4519bc196e55a137397bf68282f
Sergio Alba <sergio.alba@io.builders>
2022-11-29 10:23:11 +0100

    Cash in test
    Signed-off-by: Sergio Alba <sergio.alba@io.builders>

---
+++ sdk/__tests__/core/command/__mocks__/CashInCommandHandler.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Command } from '../../../../src/core/command/Command.js';
+import { ICommandHandler } from '../../../../src/core/command/CommandHandler.interface.js';
+import { CommandResponse } from '../../../../src/core/command/CommandResponse.interface.js';
+import { CommandHandler } from '../../../../src/core/decorator/CommandHandlerDecorator.js';
+
+export class CashInCommandResponse implements CommandResponse {
+	constructor(public readonly payload: boolean) {}
+}
+
+export class CashInCommand extends Command<CashInCommandResponse> {
+	constructor(
+		public readonly account: { id: string; privateKey?: string },
+		public readonly amount: string,
+		public readonly proxyContractId: string,
+		public readonly targetId: string,
+		public readonly tokenId: string,
+	) {
+		super();
+	}
+}
+
+export class StableCoinRepository {
+	public map = new Map<CashInCommand, CashInCommandResponse>();
+}
+
+@CommandHandler(CashInCommand)
+export class CashInCommandHandler implements ICommandHandler<CashInCommand> {
+	constructor(
+		public readonly repo: StableCoinRepository = new StableCoinRepository(),
+	) {}
+
+	execute(command: CashInCommand): Promise<CashInCommandResponse> {
+		const res = new CashInCommandResponse(true);
+		this.repo.map.set(command, res);
+		// Do some work here
+		return Promise.resolve(res);
+	}
+}
