f5fd2ce7998be47da21277a7a487c182513005d2
alberto <alberto@io.builders>
2022-12-01 08:33:00 +0100

    Decider tests added
    Signed-off-by: alberto <alberto@io.builders>

--- sdk/__tests__/port/out/handler/Decider.test.ts
+++ sdk/__tests__/port/out/handler/Decider.test.ts
@@ -2,14 +2,14 @@ import { Capability, Operation, Access } from "../../../../src/domain/context/st
 import StableCoinCapabilities from "../../../../src/domain/context/stablecoin/StableCoinCapabilities";
 import { StableCoin } from "../../../../src/domain/context/stablecoin/StableCoin";
 import  Account  from "../../../../src/domain/context/account/Account";
+import  { CapabilityDecider, Decision }  from "../../../../src/port/out/handler/decider/CapabilityDecider";
 
 
 describe('🧪 [BUILDER] HTSTransactionBuilder', () => {
 
-    const operation = Operation.CASH_IN;
-    const access = Access.CONTRACT;
-    const capabilityCashIn = new Capability(operation, access);
-    const capabilities = [capabilityCashIn];
+
+    const capabilities: Capability[] = [new Capability(Operation.CASH_IN, Access.CONTRACT),
+        new Capability(Operation.BURN, Access.HTS)];
 
     const coin = new StableCoin({
         "name": "name",
@@ -24,5 +24,28 @@ describe('🧪 [BUILDER] HTSTransactionBuilder', () => {
     const stableCoin = new StableCoinCapabilities(coin, capabilities, account);
 
     it('Test decider', async () => {
+        const CashIn_Decider =  CapabilityDecider.decide(stableCoin, Operation.CASH_IN);
+        const Burn_Decider =  CapabilityDecider.decide(stableCoin, Operation.BURN);
+        const Wipe_Decider =  CapabilityDecider.decide(stableCoin, Operation.WIPE);
+        const Freeze_Decider =  CapabilityDecider.decide(stableCoin, Operation.FREEZE);
+        const UnFreeze_Decider =  CapabilityDecider.decide(stableCoin, Operation.UNFREEZE);
+        const Pause_Decider =  CapabilityDecider.decide(stableCoin, Operation.PAUSE);
+        const UnPause_Decider =  CapabilityDecider.decide(stableCoin, Operation.UNPAUSE);
+        const Delete_Decider =  CapabilityDecider.decide(stableCoin, Operation.DELETE);
+        const Rescue_Decider =  CapabilityDecider.decide(stableCoin, Operation.RESCUE);
+        const RoleManagement_Decider =  CapabilityDecider.decide(stableCoin, Operation.ROLE_MANAGEMENT);
+
+
+        expect(CashIn_Decider).toEqual(Decision.CONTRACT);
+        expect(Burn_Decider).toEqual(Decision.HTS);
+        expect(Wipe_Decider).toEqual(Decision.FORBIDDEN);
+        expect(Freeze_Decider).toEqual(Decision.FORBIDDEN);
+        expect(UnFreeze_Decider).toEqual(Decision.FORBIDDEN);
+        expect(Pause_Decider).toEqual(Decision.FORBIDDEN);
+        expect(UnPause_Decider).toEqual(Decision.FORBIDDEN);
+        expect(Delete_Decider).toEqual(Decision.FORBIDDEN);
+        expect(Rescue_Decider).toEqual(Decision.FORBIDDEN);
+        expect(RoleManagement_Decider).toEqual(Decision.FORBIDDEN);
+
     });
 })
