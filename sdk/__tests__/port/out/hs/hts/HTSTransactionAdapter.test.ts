4aa88af1c8ff095f9571dcef7c03632239af04ab
alberto <alberto@io.builders>
2022-12-12 16:43:44 +0100

    test added
    Signed-off-by: alberto <alberto@io.builders>

--- sdk/__tests__/port/out/hs/hts/HTSTransactionAdapter.test.ts
+++ sdk/__tests__/port/out/hs/hts/HTSTransactionAdapter.test.ts
@@ -20,6 +20,10 @@ import { Network } from '../../../../../src/index.js';
 import ConnectRequest, {
 	SupportedWallets,
 } from '../../../../../src/port/in/request/ConnectRequest.js';
+import {HederaERC20AddressTestnet, FactoryAddressTestnet} from '../../../../../src/port/in/StableCoin.js';
+import PublicKey from '../../../../../src/domain/context/account/PublicKey.js';
+import ContractId from '../../../../../src/domain/context/contract/ContractId.js';
+
 
 describe('🧪 [ADAPTER] HTSTransactionAdapter with Ed25519 accounts', () => {
 	const clientAccountId = '0.0.47792863';
@@ -90,7 +94,21 @@ describe('🧪 [ADAPTER] HTSTransactionAdapter with Ed25519 accounts', () => {
 		th = Injectable.resolve(HTSTransactionAdapter);
 	});
 
-
+	it('create coin', async () => {
+		const coin = new StableCoin({
+			name: "TestCoin",
+			symbol: "TC",
+			decimals: 6,
+			adminKey: PublicKey.NULL,
+			autoRenewAccount: account.id
+		});
+		tr = await th.create(
+			coin,
+			new ContractId(FactoryAddressTestnet),
+			new ContractId(HederaERC20AddressTestnet)
+		);
+		
+	}, 50000);
 
 	it('Test cashin', async () => {
 		const accountInitialBalance: number = +(
