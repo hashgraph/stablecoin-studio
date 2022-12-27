6e1dd6e8fe0dc84da0d31e4eec968a19d5969513
alberto <alberto@io.builders>
2022-12-27 15:14:47 +0100

    Bug 400, CashIn url
    Signed-off-by: alberto <alberto@io.builders>

--- sdk/src/port/out/error/TransactionResponseError.ts
+++ sdk/src/port/out/error/TransactionResponseError.ts
@@ -3,11 +3,14 @@ import BaseError, { ErrorCode } from "../../../core/error/BaseError.js";
 const REGEX_TRANSACTION =
 	/^(0|(?:[1-9]\d*))\.(0|(?:[1-9]\d*))\.(0|(?:[1-9]\d*))(?:-([a-z]{5}))?@([1-9]\d*)\.([1-9]\d*)$/;
 const HASHSCAN_URL = 'https://hashscan.io/testnet/transaction/';
+const HASHSCAN_URL_RPC_RELAY = 'https://hashscan.io/testnet/tx/';
+
 type TransactionResponseErrorPayload = {
 	message: string;
 	name?: string;
 	status?: string;
 	transactionId?: string;
+	RPC_relay?: boolean;
 };
 
 export class TransactionResponseError extends BaseError {
@@ -17,9 +20,14 @@ export class TransactionResponseError extends BaseError {
 		super(ErrorCode.TransactionError, `Transaction failed: ${val.message}`);
 		this.error = val;
 		if (val.transactionId) {
-			const transaction =
-				val.transactionId.match(REGEX_TRANSACTION) ?? [];
-			this.transactionUrl = `${HASHSCAN_URL}${transaction[1]}.${transaction[2]}.${transaction[3]}-${transaction[5]}-${transaction[6]}`;
+			if(val.RPC_relay){
+				this.transactionUrl =`${HASHSCAN_URL_RPC_RELAY}${val.transactionId}`;
+			}
+			else{
+				const transaction =
+					val.transactionId.match(REGEX_TRANSACTION) ?? [];
+				this.transactionUrl = `${HASHSCAN_URL}${transaction[1]}.${transaction[2]}.${transaction[3]}-${transaction[5]}-${transaction[6]}`;
+			}	
 		}
 	}
 }
