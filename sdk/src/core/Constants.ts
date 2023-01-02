70274bb3433ae56ec29a2da20d05e2d8c1c1559f
Adri <adrian@io.builders>
2023-01-02 04:35:26 +0100

    fix hedera price change

--- sdk/src/core/Constants.ts
+++ sdk/src/core/Constants.ts
@@ -2,4 +2,4 @@ export const COMMAND_METADATA = '__command__';
 export const COMMAND_HANDLER_METADATA = '__commandHandler__';
 export const QUERY_METADATA = '__query__';
 export const QUERY_HANDLER_METADATA = '__queryHandler__';
-export const TOKEN_CREATION_COST_HBAR = 25;
+export const TOKEN_CREATION_COST_HBAR = 30;
