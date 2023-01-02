878959ca5e30e2287ea1d5f2f93aed5a56203ac3
Sergio Alba <sergio.alba@io.builders>
2023-01-02 16:48:24 +0100

    Add license header
    Signed-off-by: Sergio Alba <sergio.alba@io.builders>

--- sdk/resources/license-header.js
+++ sdk/resources/license-header.js
@@ -17,16 +17,3 @@
  * limitations under the License.
  *
  */
-
-import BaseError, { ErrorCode } from '../../../../core/error/BaseError.js';
-import { SupportedWallets } from '../../../../port/in/request/ConnectRequest.js';
-
-export class WalletConnectRejectedError extends BaseError {
-	constructor(wallet: SupportedWallets) {
-		super(
-			ErrorCode.PairingRejected,
-			`The user rejected the pair request for: ${wallet}`,
-		);
-		Object.setPrototypeOf(this, WalletConnectRejectedError.prototype);
-	}
-}
