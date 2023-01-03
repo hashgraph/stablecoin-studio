9976310276aa3f5ccdfd7e8f52d7893032255096
alberto <alberto@io.builders>
2023-01-03 14:51:27 +0100

    HTS through metamask fixed
    Signed-off-by: alberto <alberto@io.builders>

--- sdk/src/port/out/mirror/MirrorNodeAdapter.ts
+++ sdk/src/port/out/mirror/MirrorNodeAdapter.ts
@@ -24,6 +24,7 @@ import { singleton } from 'tsyringe';
 import StableCoinViewModel from '../../out/mirror/response/StableCoinViewModel.js';
 import AccountViewModel from '../../out/mirror/response/AccountViewModel.js';
 import StableCoinListViewModel from '../../out/mirror/response/StableCoinListViewModel.js';
+import TransactionResultViewModel from '../../out/mirror/response/TransactionResultViewModel.js';
 import { Environment } from '../../../domain/context/network/Environment.js';
 import LogService from '../../../app/service/LogService.js';
 import { StableCoinNotFound } from './error/StableCoinNotFound.js';
@@ -211,6 +212,26 @@ export class MirrorNodeAdapter {
 		}
 	}
 
+	public async getTransactionResult(
+		transactionId: string,
+	): Promise<TransactionResultViewModel> {
+		try {
+			const url = this.URI_BASE + 'contracts/results/' + transactionId
+			LogService.logTrace(url);
+			const res = await axios.get<ITransactionResult>(
+				url,
+			);
+
+			const result: TransactionResultViewModel = {
+				result: res.data.call_result.toString(),
+			};
+
+			return result;
+		} catch (error) {
+			return Promise.reject<TransactionResultViewModel>(new InvalidResponse(error));
+		}
+	}
+
 	private getMirrorNodeURL(environment: Environment): string {
 		switch (environment) {
 			case 'mainnet':
@@ -278,6 +299,10 @@ interface IAccount {
 	key: IKey;
 	alias: string;
 	account: string;
+}
+
+interface ITransactionResult {
+	call_result: string;
 }
 
 interface IKey {
