fd1cf55deab984ca33d144fc8ead1d9bf72052c1
Axel <axel@io.builders>
2023-01-05 13:23:14 +0100

    add types in deploy.ts
    Signed-off-by: Axel <axel@io.builders>

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,19 +1,11 @@
-const {
+import {
     ContractId,
-    AccountId,
     PublicKey,
     TokenSupplyType,
     PrivateKey,
     ContractFunctionParameters,
-} = require('@hashgraph/sdk')
-
-const hederaERC20Address = '' //"0.0.49127272";
-
-const factoryProxyAddress = '' //"0.0.49127286";
-const factoryProxyAdminAddress = '' //"0.0.49127281";
-const factoryAddress = '' //"0.0.49127276";
-
-const address_0 = '0x0000000000000000000000000000000000000000'
+    Client,
+} from '@hashgraph/sdk'
 
 import {
     StableCoinFactory__factory,
@@ -29,25 +21,43 @@ import {
     toEvmAddress,
 } from './utils'
 
+const hederaERC20Address = '' //"0.0.49127272";
+
+const factoryProxyAddress = '' //"0.0.49127286";
+const factoryProxyAdminAddress = '' //"0.0.49127281";
+const factoryAddress = '' //"0.0.49127276";
+
+const address_0 = '0x0000000000000000000000000000000000000000'
 const hre = require('hardhat')
 const hreConfig = hre.network.config
 
-export function initializeClients() {
+export function initializeClients(): [
+    Client,
+    string,
+    string,
+    string,
+    boolean,
+    Client,
+    string,
+    string,
+    string,
+    boolean
+] {
     const client1 = getClient()
-    const client1account = hreConfig.accounts[0].account
-    const client1privatekey = hreConfig.accounts[0].privateKey
-    const client1publickey = hreConfig.accounts[0].publicKey
-    const client1isED25519 = hreConfig.accounts[0].isED25519Type
+    const client1account: string = hreConfig.accounts[0].account
+    const client1privatekey: string = hreConfig.accounts[0].privateKey
+    const client1publickey: string = hreConfig.accounts[0].publicKey
+    const client1isED25519: boolean = hreConfig.accounts[0].isED25519Type
     client1.setOperator(
         client1account,
         toHashgraphKey(client1privatekey, client1isED25519)
     )
 
     const client2 = getClient()
-    const client2account = hreConfig.accounts[1].account
-    const client2privatekey = hreConfig.accounts[1].privateKey
-    const client2publickey = hreConfig.accounts[1].publicKey
-    const client2isED25519 = hreConfig.accounts[1].isED25519Type
+    const client2account: string = hreConfig.accounts[1].account
+    const client2privatekey: string = hreConfig.accounts[1].privateKey
+    const client2publickey: string = hreConfig.accounts[1].publicKey
+    const client2isED25519: boolean = hreConfig.accounts[1].isED25519Type
     client2.setOperator(
         client2account,
         toHashgraphKey(client2privatekey, client2isED25519)
@@ -67,33 +77,33 @@ export function initializeClients() {
     ]
 }
 
-export function toHashgraphKey(privateKey: string, isED25519: boolean): any {
+export function toHashgraphKey(privateKey: string, isED25519: boolean) {
     return isED25519
         ? PrivateKey.fromStringED25519(privateKey)
         : PrivateKey.fromStringECDSA(privateKey)
 }
 
 export function getOperatorClient(
-    client1: any,
-    client2: any,
+    client1: Client,
+    client2: Client,
     clientId: number
-): any {
+) {
     return clientId == 1 ? client1 : client2
 }
 
 export function getOperatorAccount(
-    client1account: any,
-    client2account: any,
+    client1account: string,
+    client2account: string,
     clientId: number
-): any {
+) {
     return clientId == 1 ? client1account : client2account
 }
 
 export function getOperatorPrivateKey(
-    client1privatekey: any,
-    client2privatekey: any,
+    client1privatekey: string,
+    client2privatekey: string,
     clientId: number
-): any {
+) {
     return clientId == 1 ? client1privatekey : client2privatekey
 }
 
@@ -101,39 +111,39 @@ export function getOperatorE25519(
     client1isED25519Type: boolean,
     client2isED25519Type: boolean,
     clientId: number
-): boolean {
+) {
     return clientId == 1 ? client1isED25519Type : client2isED25519Type
 }
 
 export function getOperatorPublicKey(
-    client1publickey: any,
-    client2publickey: any,
+    client1publickey: string,
+    client2publickey: string,
     clientId: number
-): any {
+) {
     return clientId == 1 ? client1publickey : client2publickey
 }
 
 export function getNonOperatorClient(
-    client1: any,
-    client2: any,
+    client1: Client,
+    client2: Client,
     clientId: number
-): any {
+) {
     return clientId == 2 ? client1 : client2
 }
 
 export function getNonOperatorAccount(
-    client1account: any,
-    client2account: any,
+    client1account: string,
+    client2account: string,
     clientId: number
-): any {
+) {
     return clientId == 2 ? client1account : client2account
 }
 
 export function getNonOperatorPrivateKey(
-    client1privatekey: any,
-    client2privatekey: any,
+    client1privatekey: string,
+    client2privatekey: string,
     clientId: number
-): any {
+) {
     return clientId == 2 ? client1privatekey : client2privatekey
 }
 
@@ -146,15 +156,15 @@ export function getNonOperatorE25519(
 }
 
 export function getNonOperatorPublicKey(
-    client1publickey: any,
-    client2publickey: any,
+    client1publickey: string,
+    client2publickey: string,
     clientId: number
-): any {
+) {
     return clientId == 2 ? client1publickey : client2publickey
 }
 
 export async function deployHederaERC20(
-    clientOperator: any,
+    clientOperator: Client,
     privateKey: string
 ) {
     // Deploying Factory logic
@@ -171,7 +181,10 @@ export async function deployHederaERC20(
     return hederaERC20
 }
 
-export async function deployFactory(clientOperator: any, privateKey: string) {
+export async function deployFactory(
+    clientOperator: Client,
+    privateKey: string
+) {
     // Deploying Factory logic
     console.log(`Deploying Contract Factory. please wait...`)
 
@@ -231,7 +244,7 @@ export async function deployContractsWithSDK(
     isED25519Type: boolean,
     freeze = false,
     allToContract = true
-) {
+): Promise<ContractId[]> {
     const AccountEvmAddress = await toEvmAddress(account, isED25519Type)
 
     console.log(
@@ -243,10 +256,10 @@ export async function deployContractsWithSDK(
     const clientSdk = getClient()
     clientSdk.setOperator(account, toHashgraphKey(privateKey, isED25519Type))
 
-    let hederaERC20: any
-    let f_address: any
-    let f_proxyAdminAddress: any
-    let f_proxyAddress: any
+    let hederaERC20: ContractId
+    let f_address: ContractId
+    let f_proxyAdminAddress: ContractId
+    let f_proxyAddress: ContractId
 
     // Deploying HederaERC20 or using an already deployed one
     if (!hederaERC20Address) {
