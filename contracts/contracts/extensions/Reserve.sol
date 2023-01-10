86cac149986346b6f92da0a460c535c9b3d2db59
Axel <axel@io.builders>
2023-01-10 15:05:05 +0100

    fix reserve contracts
    Signed-off-by: Axel <axel@io.builders>

--- contracts/contracts/extensions/Reserve.sol
+++ contracts/contracts/extensions/Reserve.sol
@@ -29,14 +29,15 @@ abstract contract Reserve is IReserve, TokenOwner, Roles {
         _;
     }
 
-    function _initReserve(address dataFeed) internal {
+    function reserve_init(address dataFeed) internal {
         _dataFeed = dataFeed;
     }
 
-    function _checkReserve(
-        uint256 amount,
-        bool less
-    ) internal view returns (bool) {
+    function _checkReserve(uint256 amount, bool less)
+        internal
+        view
+        returns (bool)
+    {
         uint256 currentReserve = getReserve();
         if (less) {
             return currentReserve >= amount;
@@ -57,9 +58,10 @@ abstract contract Reserve is IReserve, TokenOwner, Roles {
         }
     }
 
-    function updateDataFeed(
-        address newAddress
-    ) external onlyRole(_getRoleId(roleName.ADMIN)) {
+    function updateDataFeed(address newAddress)
+        external
+        onlyRole(_getRoleId(roleName.ADMIN))
+    {
         address previous = _dataFeed;
         _dataFeed = newAddress;
         emit ReserveAddressChanged(previous, newAddress);
