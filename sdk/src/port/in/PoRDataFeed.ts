862013ce53ea13d47f2f583912669cee9d686573
mamorales <miguelangel@io.builders>
2023-01-11 10:47:31 +0100

    Naming change
    Signed-off-by: mamorales <miguelangel@io.builders>

---
+++ sdk/src/port/in/PoRDataFeed.ts
@@ -0,0 +1,78 @@
+/*
+ *
+ * Hedera Stable Coin SDK
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Injectable from '../../core/Injectable.js';
+import { PoRAmountDecimals } from './request/CreateRequest.js';
+import BigDecimal from '../../domain/context/shared/BigDecimal.js';
+import ContractId from '../../domain/context/contract/ContractId.js';
+import { CommandBus } from '../../core/command/CommandBus.js';
+import { handleValidation } from './Common.js';
+import UpdatePoRAmountRequest from './request/UpdatePoRAmountRequest.js';
+import { UpdatePoRAmountCommand } from '../../app/usecase/command/PoRdatafeed/operations/updatePoRAmount/UpdatePoRAmountCommand.js';
+import GetPoRAmountRequest from './request/GetPoRAmountRequest.js';
+import { GetPoRAmountCommand } from '../../app/usecase/command/PoRdatafeed/operations/getPoRAmount/GetPoRAmountCommand.js';
+import { Balance } from '../../domain/context/stablecoin/Balance.js';
+import { HederaId } from '../../domain/context/shared/HederaId.js';
+
+interface IPoRDataFeedInPort {
+	getPoRAmount(request: GetPoRAmountRequest): Promise<Balance>;
+	updatePoRAmount(request: UpdatePoRAmountRequest): Promise<boolean>
+}
+
+class PoRDataFeedInPort implements IPoRDataFeedInPort {
+	constructor(
+		private readonly commandBus: CommandBus = Injectable.resolve(
+			CommandBus,
+		)
+	) {}
+
+	async getPoRAmount(
+		request: GetPoRAmountRequest,
+	): Promise<Balance> {
+		handleValidation('GetPoRAmountRequest', request);
+
+		const res = await this.commandBus.execute(
+			new GetPoRAmountCommand(
+				HederaId.from(request.tokenId)
+			)
+		);
+
+		return new Balance(res.payload);
+	}
+
+	async updatePoRAmount(
+		request: UpdatePoRAmountRequest,
+	): Promise<boolean> {
+		handleValidation('UpdatePoRAmountRequest', request);
+
+		return (
+			await this.commandBus.execute(
+				new UpdatePoRAmountCommand(
+					new ContractId(request.PoR),
+					BigDecimal.fromString(request.PoRAmount, PoRAmountDecimals)
+				),
+			)
+		).payload;
+	}
+}
+
+const PoRDataFeed = new PoRDataFeedInPort();
+export default PoRDataFeed;
