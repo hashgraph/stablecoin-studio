7b4f8d035d929eeb38e1ccaf1ab645333d3db3ca
Major <110887433+sergioaiobuilders@users.noreply.github.com>
2023-01-16 15:18:19 +0100

    getSupplierAllowance neede
    Signed-off-by: Major <110887433+sergioaiobuilders@users.noreply.github.com>

---
+++ web/src/views/StableCoinProof/__test__/index.test.tsx
@@ -0,0 +1,64 @@
+
+import { render } from '../../../test/index';
+import translations from '../../../translations/en/StableCoinProof.json';
+import configureMockStore from 'redux-mock-store';
+import StableCoinProof from '../';
+
+const mockStore = configureMockStore();
+
+describe(`<${StableCoinProof.name} />`, () => {
+	beforeEach(() => {});
+
+	test('should render correctly', () => {
+		const component = render(<StableCoinProof />);
+
+		expect(component.asFragment()).toMatchSnapshot();
+	});
+
+	test('should has title', () => {
+		const component = render(<StableCoinProof />);
+		const header = component.getByTestId('base-container-heading');
+
+		expect(header).toHaveTextContent(translations.title);
+	});
+
+	test('should has subtitle', () => {
+		const selectedStableCoin = {
+			initialSupply: 0,
+			tokenId: '0.0.48162226',
+			totalSupply: 0,
+			name: 'MIDAS',
+			symbol: 'MD',
+			decimals: 3,
+			id: '0.0.48132286',
+			maxSupply: '100000',
+			treasuryId: '0.0.48160285',
+			memo: 'Hedera Accelerator Stable Coin',
+			adminKey: {
+				key: 'key',
+				type: 'ED25519',
+			},
+			freezeKey: {
+				key: 'key',
+				type: 'ED25519',
+			},
+			wipeKey: {
+				id: '0.0.48160285',
+			},
+			supplyKey: {
+				id: '0.0.48160285',
+			},
+		};
+
+		const store = mockStore({
+			wallet: {
+				selectedStableCoin,
+			},
+		});
+
+		const component = render(<StableCoinProof />, store);
+		const subtitle = component.getByTestId('details-review-title');
+
+		expect(subtitle).toHaveTextContent(translations.subtitle);
+	});
+});
