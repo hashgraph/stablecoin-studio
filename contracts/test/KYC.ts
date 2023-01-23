8a36e1eea17774c9520e0c568dd6d13fcc461c92
Axel <axel@io.builders>
2023-01-23 18:00:28 +0100

    Add test KYC
    Signed-off-by: Axel <axel@io.builders>

--- contracts/test/KYC.ts
+++ contracts/test/KYC.ts
@@ -37,6 +37,8 @@ import {
     transferFrom,
     Burn,
     transfer,
+    grantKyc,
+    revokeKyc,
 } from '../scripts/contractsMethods'
 import { clientId, toEvmAddress } from '../scripts/utils'
 import { Client, ContractId } from '@hashgraph/sdk'
@@ -138,6 +140,453 @@ describe('HederaERC20 Tests', function() {
         })
 
         proxyAddress = result[0]
-        reserveProxy = result[6]
+    })
+
+    it('An account without kyc can not cash in', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await expect(
+            Mint(
+                proxyAddress,
+                amount,
+                operatorClient,
+                operatorAccount,
+                operatorIsE25519
+            )
+        ).to.eventually.be.rejectedWith(Error)
+    })
+
+    it('An account with kyc can cash in', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await expect(
+            Mint(
+                proxyAddress,
+                amount,
+                operatorClient,
+                operatorAccount,
+                operatorIsE25519
+            )
+        ).to.eventually.be.rejectedWith(Error)
+
+        const balanceBefore = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+
+        const balanceAfter = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        expect(balanceBefore.add(amount).toString()).to.equals(
+            balanceAfter.toString()
+        )
+
+        // RESET
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        const balanceAfterWipe = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        expect(balanceAfterWipe.toString()).to.equals('0')
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+    })
+
+    it('An account without kyc can not wipe', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+
+        await expect(
+            Wipe(
+                proxyAddress,
+                amount,
+                operatorClient,
+                operatorAccount,
+                operatorIsE25519
+            )
+        ).to.eventually.be.rejectedWith(Error)
+
+        // RESET
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        const balanceAfterWipe = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        expect(balanceAfterWipe.toString()).to.equals('0')
+    })
+
+    it('An account with kyc can wipe', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+
+        const balanceBeforeWipe = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        const balanceAfterWipe = await getBalanceOf(
+            proxyAddress,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        expect(balanceBeforeWipe.sub(amount).toString()).to.equals(
+            balanceAfterWipe.toString()
+        )
+
+        // RESET
+
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+    })
+
+    it('An account with kyc can not transfer tokens to an account without kyc', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await associateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await expect(
+            transfer(
+                proxyAddress,
+                nonOperatorAccount,
+                nonOperatorIsE25519,
+                amount,
+                operatorClient
+            )
+        ).to.eventually.be.rejectedWith(Error)
+
+        // RESET
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+    })
+
+    it('An account with kyc can transfer token to other account with kyc', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await associateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await grantKyc(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            operatorClient
+        )
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await transfer(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            amount,
+            operatorClient
+        )
+        const balance = await getBalanceOf(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+
+        expect(balance.toString()).to.equal(amount.toString())
+        // RESET
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await revokeKyc(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            operatorClient
+        )
+        await dissociateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+    })
+
+    it('An account with kyc, approve to transfer to other account without kyc', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await associateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await approve(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            amount,
+            operatorClient
+        )
+        await expect(
+            transferFrom(
+                proxyAddress,
+                operatorAccount,
+                operatorIsE25519,
+                nonOperatorAccount,
+                nonOperatorIsE25519,
+                amount,
+                nonOperatorClient
+            )
+        ).to.eventually.be.rejectedWith(Error)
+
+        // RESET
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await approve(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            BigNumber.from(0),
+            operatorClient
+        )
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await dissociateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+    })
+
+    it.only('An account with kyc, approve to transfer to other account with kyc', async () => {
+        const amount = BigNumber.from(1).mul(TokenFactor)
+        await grantKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+
+        await associateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await grantKyc(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            operatorClient
+        )
+        await Mint(
+            proxyAddress,
+            amount,
+            operatorClient,
+            operatorAccount,
+            operatorIsE25519
+        )
+        await approve(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            amount,
+            operatorClient
+        )
+        await transferFrom(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            amount,
+            nonOperatorClient
+        )
+
+        // RESET
+        await Wipe(
+            proxyAddress,
+            amount,
+            operatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
+        await revokeKyc(
+            proxyAddress,
+            operatorAccount,
+            operatorIsE25519,
+            operatorClient
+        )
+        await revokeKyc(
+            proxyAddress,
+            nonOperatorAccount,
+            nonOperatorIsE25519,
+            operatorClient
+        )
+        await dissociateToken(
+            proxyAddress,
+            nonOperatorClient,
+            nonOperatorAccount,
+            nonOperatorIsE25519
+        )
     })
 })
