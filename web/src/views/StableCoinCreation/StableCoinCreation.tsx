301f53efcd450b474a3b0009ddaef2413f1245d9
Miguel Ángel <miguelangel@io.builders>
2023-04-17 15:06:05 +0200

    disable button on management permission step
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- web/src/views/StableCoinCreation/StableCoinCreation.tsx
+++ web/src/views/StableCoinCreation/StableCoinCreation.tsx
@@ -149,8 +149,7 @@ const StableCoinCreation = () => {
 				const keys = ['adminKey', 'supplyKey', 'wipeKey', 'freezeKey', 'pauseKey', 'kycKey'];
 
 				// @ts-ignore
-				fieldsStep = watch(keys);
-				fieldsStep.forEach((item, index) => {
+				watch(keys).forEach((item, index) => {
 					if (item?.value === OTHER_KEY_VALUE) {
 						// @ts-ignore
 						fieldsStep[index] = watch(keys[index].concat('Other'));
