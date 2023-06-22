7a188c614153c2037a84eadd68d021091295f2bf
Miguel Ángel <miguelangel@io.builders>
2023-06-22 12:05:18 +0200

    Increase gas por update token operation
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- sdk/__tests__/port/in/StableCoin.test.ts
+++ sdk/__tests__/port/in/StableCoin.test.ts
@@ -66,6 +66,7 @@ import {
 	CLIENT_ACCOUNT_ECDSA,
 	FACTORY_ADDRESS,
 	HEDERA_TOKEN_MANAGER_ADDRESS,
+	CLIENT_ACCOUNT_ID_ED25519,
 } from '../../config.js';
 import { Client, Hbar, TransferTransaction } from '@hashgraph/sdk';
 import { MirrorNode } from '../../../src/domain/context/network/MirrorNode.js';
