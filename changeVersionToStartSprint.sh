1480b885c0e7871d5601f0ddad0a2593a9a2452d
Adri <adrian@io.builders>
2023-06-26 15:16:49 +0200

    Open sprint 21 and scripts

---
+++ changeVersionToStartSprint.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph-dev\/stablecoin-npm-sdk\": \".\/..\/sdk\"/g"
+
+find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph-dev\/stablecoin-npm-contracts\": \".\/..\/contracts\"/g" 
+
+find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/hashconnect\": \".*\"/\"@hashgraph-dev\/hashconnect\": \".\/..\/hasconnect\/lib\"/g"
+
+
+
