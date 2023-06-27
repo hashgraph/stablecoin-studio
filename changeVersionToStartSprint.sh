aa4d2ffc695e391f385fd0b2cc944f2a808e03eb
Adri <adrian@io.builders>
2023-06-27 10:14:49 +0200

    solve error in library version

--- changeVersionToStartSprint.sh
+++ changeVersionToStartSprint.sh
@@ -5,7 +5,7 @@ find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v no
 
 find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph-dev\/stablecoin-npm-contracts\": \".\/..\/contracts\"/g" 
 
-find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/hashconnect\": \".*\"/\"@hashgraph-dev\/hashconnect\": \".\/..\/hasconnect\/lib\"/g"
+find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph-dev\/hashconnect\": \".*\"/\"@hashgraph-dev\/hashconnect\": \".\/..\/hashconnect\/lib\"/g"
 
 
 
