c0b4d3cd937175b73d6014ff29f5b980f0692361
Miguel Ángel <miguelangel@io.builders>
2023-07-12 09:00:31 +0200

    SetMirrorNodeService tests changes
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- cli/__tests__/app/service/configuration/SetMirrorNodeService.test.ts
+++ cli/__tests__/app/service/configuration/SetMirrorNodeService.test.ts
@@ -6,7 +6,8 @@ import {
 import Language from '../../../../src/domain/language/Language.js';
 import { IMirrorsConfig } from '../../../../src/domain/configuration/interfaces/IMirrorsConfig.js';
 import { IConfiguration } from '../../../../src/domain/configuration/interfaces/IConfiguration.js';
-// import { Network } from '@hashgraph-dev/stablecoin-npm-sdk';
+import { Network } from '@hashgraph-dev/stablecoin-npm-sdk';
+// import WizardService from '../../../../src/app/service/wizard/WizardService.js';
 
 const language: Language = new Language();
 
@@ -76,29 +77,25 @@ describe('setMirrorNodeService', () => {
   it('should configure mirror node', async () => {
     const defaultSingleAskMock = jest
       .spyOn(utilsService, 'defaultSingleAsk')
-      .mockImplementation((question: string) => {
-        switch (question) {
-          case language.getText('configuration.askName'):
-            return Promise.resolve('LOCAL');
-
-          case language.getText('configuration.askBaseUrl'):
-            return Promise.resolve('https://127.0.0.1:2746/api/');
-
-          case language.getText('configuration.askApiKey'):
-            return Promise.resolve('');
-
-          case language.getText('configuration.askHeaderName'):
-            return Promise.resolve('');
-
-          default:
-            return Promise.resolve('');
-        }
-      });
+      .mockImplementationOnce(() => Promise.resolve('HEDERA'))
+      .mockImplementationOnce(() => Promise.resolve('LOCAL'))
+      .mockImplementationOnce(() => Promise.resolve('test'))
+      .mockImplementationOnce(() =>
+        Promise.resolve('https://testnet.mirrornode.hedera.com/api/v1/'),
+      )
+      .mockImplementationOnce(() =>
+        Promise.resolve('https://127.0.0.1:2746/api/'),
+      )
+      .mockImplementationOnce(() => Promise.resolve(''))
+      .mockImplementationOnce(() => Promise.resolve(''));
 
     const defaultConfirmAskMock = jest
       .spyOn(utilsService, 'defaultConfirmAsk')
       .mockImplementation((question: string) => {
         switch (question) {
+          case language.getText('configuration.askNeedApiKey'):
+            return Promise.resolve(true);
+
           case language.getText('configuration.askCreateConfig'):
             return Promise.resolve(false);
 
@@ -118,9 +115,24 @@ describe('setMirrorNodeService', () => {
       await setMirrorNodeService.configureMirrorNode('testnet');
 
     expect(setMirrorNodeService).not.toBeNull();
-    expect(defaultSingleAskMock).toHaveBeenCalledTimes(2);
+    expect(defaultSingleAskMock).toHaveBeenCalledTimes(7);
     expect(defaultConfirmAskMock).toHaveBeenCalledTimes(2);
     expect(mirrorsConfig).not.toBeNull();
+  });
+
+  it('should get default mirror by network', async () => {
+    const mirrorsConfig: IMirrorsConfig =
+      setMirrorNodeService.getDefaultMirrorByNetwork('testnet');
+
+    expect(setMirrorNodeService).not.toBeNull();
+    expect(mirrorsConfig.network).toBe('testnet');
+    expect(mirrorsConfig.name).toBe('HEDERA');
+    expect(mirrorsConfig.baseUrl).toBe(
+      'https://testnet.mirrornode.hedera.com/api/v1/',
+    );
+    expect(mirrorsConfig.apiKey).toBeUndefined();
+    expect(mirrorsConfig.headerName).toBeUndefined();
+    expect(mirrorsConfig.selected).toBe(true);
   });
 
   it('should remove mirror node', async () => {
@@ -195,51 +207,136 @@ describe('setMirrorNodeService', () => {
 
     expect(setMirrorNodeService).not.toBeNull();
     expect(defaultMultipleAskMock).toHaveBeenCalledTimes(2);
-    expect(defaultSingleAskMock).toHaveBeenCalledTimes(4);
+    expect(defaultSingleAskMock).toHaveBeenCalledTimes(9);
     expect(defaultConfirmAskMock).toHaveBeenCalledTimes(6);
   });
 
-  /* it('should manage mirror menu', async () => {
-        const currentNetworkMock = jest
-        .spyOn(utilsService, 'getCurrentNetwork')
-        .mockReturnValue(configurationMock.networks[0]);
+  it('should list mirror nodes', async () => {
+    const currentNetworkMock = jest
+      .spyOn(utilsService, 'getCurrentNetwork')
+      .mockReturnValue(configurationMock.networks[0]);
 
-        const currentAccountMock = jest
-          .spyOn(utilsService, 'getCurrentAccount')
-          .mockReturnValue(configurationMock.accounts[0]);
-  
-        const currentMirrorMock = jest
-          .spyOn(utilsService, 'getCurrentMirror')
-          .mockReturnValue(configurationMock.mirrors[0]);
-    
-        const currentRPCMock = jest
-          .spyOn(utilsService, 'getCurrentRPC')
-          .mockReturnValue(configurationMock.rpcs[0]);
+    const currentAccountMock = jest
+      .spyOn(utilsService, 'getCurrentAccount')
+      .mockReturnValue(configurationMock.accounts[0]);
 
-        const networkConnectMock = jest
-          .spyOn(Network, 'connect')
-          .mockImplementation(() => Promise.resolve({}));
+    const currentMirrorMock = jest
+      .spyOn(utilsService, 'getCurrentMirror')
+      .mockReturnValue(configurationMock.mirrors[0]);
 
-        const getConfigurationMock = jest
-          .spyOn(configurationService, 'getConfiguration')
-          .mockReturnValue(configurationMock);          
+    const currentRPCMock = jest
+      .spyOn(utilsService, 'getCurrentRPC')
+      .mockReturnValue(configurationMock.rpcs[0]);
 
-        const defaultMultipleAskMock = jest
-          .spyOn(utilsService, 'defaultMultipleAsk')
-          .mockImplementationOnce(() => Promise.resolve(language.getText('wizard.manageMirrorNodeOptions.Change')))   
-          .mockImplementationOnce(() => Promise.resolve('ARKHIA'));    
+    const networkConnectMock = jest
+      .spyOn(Network, 'connect')
+      .mockImplementation(() => Promise.resolve({}));
 
-        await setMirrorNodeService.manageMirrorNodeMenu('testnet');
-  
-        expect(setMirrorNodeService).not.toBeNull();
-        expect(currentNetworkMock).toHaveBeenCalledTimes(1);
-        expect(currentAccountMock).toHaveBeenCalledTimes(1);
-        expect(currentMirrorMock).toHaveBeenCalledTimes(1);
-        expect(currentRPCMock).toHaveBeenCalledTimes(1);
-        expect(getConfigurationMock).toHaveBeenCalledTimes(1);
-        expect(defaultMultipleAskMock).toHaveBeenCalledTimes(1);
-        expect(networkConnectMock).toHaveBeenCalledTimes(1);
-    });  */
+    const getConfigurationMock = jest
+      .spyOn(configurationService, 'getConfiguration')
+      .mockReturnValue(configurationMock);
+
+    /*const configurationMenuMock = jest
+        .spyOn(WizardService.prototype, 'configurationMenu')
+        .mockImplementation();  */
+
+    const defaultMultipleAskMock = jest
+      .spyOn(utilsService, 'defaultMultipleAsk')
+      .mockImplementationOnce(() =>
+        Promise.resolve(
+          language.getText('wizard.manageMirrorNodeOptions.List'),
+        ),
+      );
+
+    await setMirrorNodeService.manageMirrorNodeMenu('testnet');
+
+    expect(setMirrorNodeService).not.toBeNull();
+    expect(currentNetworkMock).toHaveBeenCalledTimes(1);
+    expect(currentAccountMock).toHaveBeenCalledTimes(1);
+    expect(currentMirrorMock).toHaveBeenCalledTimes(1);
+    expect(currentRPCMock).toHaveBeenCalledTimes(1);
+    expect(getConfigurationMock).toHaveBeenCalledTimes(1);
+    // expect(configurationMenuMock).toHaveBeenCalledTimes(1);
+    expect(defaultMultipleAskMock).toHaveBeenCalledTimes(1);
+    expect(networkConnectMock).toHaveBeenCalledTimes(1);
+  });
+
+  /* it('should add mirror nodes', async () => {
+    const currentNetworkMock = jest
+    .spyOn(utilsService, 'getCurrentNetwork')
+    .mockReturnValue(configurationMock.networks[0]);
+
+    const currentAccountMock = jest
+      .spyOn(utilsService, 'getCurrentAccount')
+      .mockReturnValue(configurationMock.accounts[0]);
+
+    const currentMirrorMock = jest
+      .spyOn(utilsService, 'getCurrentMirror')
+      .mockReturnValue(configurationMock.mirrors[0]);
+
+    const currentRPCMock = jest
+      .spyOn(utilsService, 'getCurrentRPC')
+      .mockReturnValue(configurationMock.rpcs[0]);
+
+    const networkConnectMock = jest
+      .spyOn(Network, 'connect')
+      .mockImplementation(() => Promise.resolve({}));
+
+    const getConfigurationMock = jest
+      .spyOn(configurationService, 'getConfiguration')
+      .mockReturnValue(configurationMock);          
+
+    const defaultMultipleAskMock = jest
+      .spyOn(utilsService, 'defaultMultipleAsk')
+      .mockImplementationOnce(() => Promise.resolve(language.getText('wizard.manageMirrorNodeOptions.Add')))   
+      .mockImplementationOnce(() => Promise.resolve(language.getText('wizard.manageMirrorNodeOptions.Change')))           
+      .mockImplementationOnce(() => Promise.resolve('ARKHIA2'));
+      
+    const defaultSingleAskMock = jest
+      .spyOn(utilsService, 'defaultSingleAsk')
+      .mockImplementation((question: string) => {
+        switch (question) {
+          case language.getText('configuration.askName'):
+            return Promise.resolve('ARKHIA2');
+
+          case language.getText('configuration.askBaseUrl'):
+            return Promise.resolve(
+              'https://starter.arkhia2.io/hedera/testnet/api/v1',
+          );
+
+          default:
+            return Promise.resolve('');
+        }
+    });
+
+    const defaultConfirmAskMock = jest
+    .spyOn(utilsService, 'defaultConfirmAsk')
+    .mockImplementation((question: string) => {
+      switch (question) {
+        case language.getText('configuration.askNeedApiKey'):
+          return Promise.resolve(false);
+
+        case language.getText('configuration.askOperateWithNewMirrorNode'):
+          return Promise.resolve(true);
+
+        default:
+          return Promise.resolve(false);
+      }
+    });
+        
+    await setMirrorNodeService.manageMirrorNodeMenu('testnet');
+
+    expect(setMirrorNodeService).not.toBeNull();
+    expect(currentNetworkMock).toHaveBeenCalledTimes(1);
+    expect(currentAccountMock).toHaveBeenCalledTimes(1);
+    expect(currentMirrorMock).toHaveBeenCalledTimes(1);
+    expect(currentRPCMock).toHaveBeenCalledTimes(1);
+    expect(getConfigurationMock).toHaveBeenCalledTimes(1);
+    expect(defaultMultipleAskMock).toHaveBeenCalledTimes(1);
+    expect(defaultSingleAskMock).toHaveBeenCalledTimes(1);
+    expect(defaultConfirmAskMock).toHaveBeenCalledTimes(1);
+    expect(networkConnectMock).toHaveBeenCalledTimes(1);
+  }, 10000); */
 
   afterAll(() => {
     jest.restoreAllMocks();
