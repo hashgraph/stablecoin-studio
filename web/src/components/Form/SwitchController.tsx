e13e503b6c018baae32703b2401212c4fe0fe300
Miguel Ángel <miguelangel@io.builders>
2023-08-23 16:30:42 +0200

    Tooltip in UI PoR step of stable coin creation process
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- web/src/components/Form/SwitchController.tsx
+++ web/src/components/Form/SwitchController.tsx
@@ -1,15 +1,18 @@
 import Switch from './Switch';
 import { Controller } from 'react-hook-form';
 import type { Control, FieldValues } from 'react-hook-form';
+import { Flex, FormControl, Link, Stack } from '@chakra-ui/react';
+import { InfoIcon } from '@chakra-ui/icons';
 
 export interface SwitchControllerProps {
 	control: Control<FieldValues>;
 	defaultValue?: boolean;
 	disabled?: boolean;
 	name: string;
+	link?: string | null;
 }
 
-const SwitchController = (props: SwitchControllerProps) => {
+const SwitchController = ({ link, ...props }: SwitchControllerProps) => {
 	const { control, defaultValue = true, disabled, name } = props;
 
 	return (
@@ -18,7 +21,18 @@ const SwitchController = (props: SwitchControllerProps) => {
 			defaultValue={defaultValue}
 			name={name}
 			render={({ field: { onChange, value } }) => (
-				<Switch checked={value} disabled={disabled} onChange={() => onChange(!value)} />
+				<Stack w='full'>
+					<FormControl>
+						<Flex justifyContent='start'>
+							{link && (
+								<Link href={link} isExternal>
+									<InfoIcon />
+								</Link>
+							)}
+						</Flex>
+						<Switch checked={value} disabled={disabled} onChange={() => onChange(!value)} />
+					</FormControl>
+				</Stack>
 			)}
 		/>
 	);
