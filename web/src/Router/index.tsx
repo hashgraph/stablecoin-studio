555e2f718e809ef67b30839cb8b391a349c03bcf
Miguel Ángel <miguelangel@io.builders>
2023-08-29 11:00:40 +0200

    Operations disabled in UI
    Signed-off-by: Miguel Ángel <miguelangel@io.builders>

--- web/src/Router/index.tsx
+++ web/src/Router/index.tsx
@@ -80,63 +80,53 @@ const Operations = () => {
 
 		const operations = capabilities?.capabilities.map((x) => x.operation);
 		const areDisabled = {
-			cashIn: !isExternalToken
-				? !operations?.includes(Operation.CASH_IN) || !roles.includes(StableCoinRole.CASHIN_ROLE)
-				: !operations?.includes(Operation.CASH_IN) ||
-				  (operations?.includes(Operation.CASH_IN) &&
-						getAccessByOperation(Operation.CASH_IN) !== Access.HTS &&
-						!roles.includes(StableCoinRole.CASHIN_ROLE)),
-			burn: !isExternalToken
-				? !operations?.includes(Operation.BURN) || !roles.includes(StableCoinRole.BURN_ROLE)
-				: !operations?.includes(Operation.BURN) ||
-				  (operations?.includes(Operation.BURN) &&
-						getAccessByOperation(Operation.BURN) !== Access.HTS &&
-						!roles.includes(StableCoinRole.BURN_ROLE)),
+			cashIn:
+				!operations?.includes(Operation.CASH_IN) ||
+				(operations?.includes(Operation.CASH_IN) &&
+					getAccessByOperation(Operation.CASH_IN) !== Access.HTS &&
+					!roles.includes(StableCoinRole.CASHIN_ROLE)),
+			burn:
+				!operations?.includes(Operation.BURN) ||
+				(operations?.includes(Operation.BURN) &&
+					getAccessByOperation(Operation.BURN) !== Access.HTS &&
+					!roles.includes(StableCoinRole.BURN_ROLE)),
 			balance: false,
-			rescue: !isExternalToken
-				? !operations?.includes(Operation.RESCUE) || !roles.includes(StableCoinRole.RESCUE_ROLE)
-				: !operations?.includes(Operation.RESCUE) ||
-				  (operations?.includes(Operation.RESCUE) &&
-						getAccessByOperation(Operation.RESCUE) !== Access.HTS &&
-						!roles.includes(StableCoinRole.RESCUE_ROLE)),
-			rescueHBAR: !isExternalToken
-				? !operations?.includes(Operation.RESCUE_HBAR) ||
-				  !roles.includes(StableCoinRole.RESCUE_ROLE)
-				: !operations?.includes(Operation.RESCUE_HBAR) ||
-				  (operations?.includes(Operation.RESCUE_HBAR) &&
-						getAccessByOperation(Operation.RESCUE_HBAR) !== Access.HTS &&
-						!roles.includes(StableCoinRole.RESCUE_ROLE)),
-			wipe: !isExternalToken
-				? !operations?.includes(Operation.WIPE) || !roles.includes(StableCoinRole.WIPE_ROLE)
-				: !operations?.includes(Operation.WIPE) ||
-				  (operations?.includes(Operation.WIPE) &&
-						getAccessByOperation(Operation.WIPE) !== Access.HTS &&
-						!roles.includes(StableCoinRole.WIPE_ROLE)),
-			freeze: !isExternalToken
-				? !operations?.includes(Operation.FREEZE) || !roles.includes(StableCoinRole.FREEZE_ROLE)
-				: !operations?.includes(Operation.FREEZE) ||
-				  (operations?.includes(Operation.FREEZE) &&
-						getAccessByOperation(Operation.FREEZE) !== Access.HTS &&
-						!roles.includes(StableCoinRole.FREEZE_ROLE)),
+			rescue:
+				!operations?.includes(Operation.RESCUE) ||
+				(operations?.includes(Operation.RESCUE) &&
+					getAccessByOperation(Operation.RESCUE) !== Access.HTS &&
+					!roles.includes(StableCoinRole.RESCUE_ROLE)),
+			rescueHBAR:
+				!operations?.includes(Operation.RESCUE_HBAR) ||
+				(operations?.includes(Operation.RESCUE_HBAR) &&
+					getAccessByOperation(Operation.RESCUE_HBAR) !== Access.HTS &&
+					!roles.includes(StableCoinRole.RESCUE_ROLE)),
+			wipe:
+				!operations?.includes(Operation.WIPE) ||
+				(operations?.includes(Operation.WIPE) &&
+					getAccessByOperation(Operation.WIPE) !== Access.HTS &&
+					!roles.includes(StableCoinRole.WIPE_ROLE)),
+			freeze:
+				!operations?.includes(Operation.FREEZE) ||
+				(operations?.includes(Operation.FREEZE) &&
+					getAccessByOperation(Operation.FREEZE) !== Access.HTS &&
+					!roles.includes(StableCoinRole.FREEZE_ROLE)),
 			checkFrozen: false,
-			pause: !isExternalToken
-				? !operations?.includes(Operation.PAUSE) || !roles.includes(StableCoinRole.PAUSE_ROLE)
-				: !operations?.includes(Operation.PAUSE) ||
-				  (operations?.includes(Operation.PAUSE) &&
-						getAccessByOperation(Operation.PAUSE) !== Access.HTS &&
-						!roles.includes(StableCoinRole.PAUSE_ROLE)),
-			delete: !isExternalToken
-				? !operations?.includes(Operation.DELETE) || !roles.includes(StableCoinRole.DELETE_ROLE)
-				: !operations?.includes(Operation.DELETE) ||
-				  (operations?.includes(Operation.DELETE) &&
-						getAccessByOperation(Operation.DELETE) !== Access.HTS &&
-						!roles.includes(StableCoinRole.DELETE_ROLE)),
-			kyc: !isExternalToken
-				? !operations?.includes(Operation.GRANT_KYC) || !roles.includes(StableCoinRole.KYC_ROLE)
-				: !operations?.includes(Operation.GRANT_KYC) ||
-				  (operations?.includes(Operation.GRANT_KYC) &&
-						getAccessByOperation(Operation.GRANT_KYC) !== Access.HTS &&
-						!roles.includes(StableCoinRole.KYC_ROLE)),
+			pause:
+				!operations?.includes(Operation.PAUSE) ||
+				(operations?.includes(Operation.PAUSE) &&
+					getAccessByOperation(Operation.PAUSE) !== Access.HTS &&
+					!roles.includes(StableCoinRole.PAUSE_ROLE)),
+			delete:
+				!operations?.includes(Operation.DELETE) ||
+				(operations?.includes(Operation.DELETE) &&
+					getAccessByOperation(Operation.DELETE) !== Access.HTS &&
+					!roles.includes(StableCoinRole.DELETE_ROLE)),
+			kyc:
+				!operations?.includes(Operation.GRANT_KYC) ||
+				(operations?.includes(Operation.GRANT_KYC) &&
+					getAccessByOperation(Operation.GRANT_KYC) !== Access.HTS &&
+					!roles.includes(StableCoinRole.KYC_ROLE)),
 			checkKyc: selectedStableCoin?.kycKey === undefined,
 		};
 		setDisabledFeatures(areDisabled);
--- web/src/views/StableCoinCreation/StableCoinCreation.tsx
+++ web/src/views/StableCoinCreation/StableCoinCreation.tsx
@@ -309,19 +309,9 @@ const StableCoinCreation = () => {
 			? formatKey(feeScheduleKey.label, 'feeScheduleKey')
 			: undefined;
 
-		request.cashInRoleAccount = formatRoleAccountByKey(
-			managementPermissions,
-			supplyKey,
-			cashInRoleAccount,
-			'cashIn',
-		);
+		request.cashInRoleAccount = formatRoleAccount(cashInRoleAccount, 'cashIn');
 		request.cashInRoleAllowance = cashInAllowanceType ? '0' : cashInAllowance;
-		request.burnRoleAccount = formatRoleAccountByKey(
-			managementPermissions,
-			supplyKey,
-			burnRoleAccount,
-			'burn',
-		);
+		request.burnRoleAccount = formatRoleAccount(burnRoleAccount, 'burn');
 		request.wipeRoleAccount = formatRoleAccountByKey(
 			managementPermissions,
 			wipeKey,
@@ -341,12 +331,7 @@ const StableCoinCreation = () => {
 			freezeRoleAccount,
 			'freeze',
 		);
-		request.deleteRoleAccount = formatRoleAccountByKey(
-			managementPermissions,
-			adminKey,
-			deleteRoleAccount,
-			'delete',
-		);
+		request.deleteRoleAccount = formatRoleAccount(deleteRoleAccount, 'delete');
 		request.kycRoleAccount = formatKycRoleAccountByKey(kycRequired, kycKey, kycRoleAccount, 'kyc');
 
 		request.hederaTokenManager = hederaTokenManagerId.value;
