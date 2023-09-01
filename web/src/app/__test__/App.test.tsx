aaf85d210d64a1bd6ef758dbfd91bbc69fd57018
Kenny Gomez <kenny@io.builders>
2023-09-01 13:36:56 +0200

    [UI] When a user opens the UI through a smartphone or tablet, display an error#813

--- web/src/app/__test__/App.test.tsx
+++ web/src/app/__test__/App.test.tsx
@@ -1,9 +1,21 @@
 import { render } from '@testing-library/react';
 import App from '../App';
+import * as reactDeviceDetect from 'react-device-detect';
 
 describe(`<${App.name} />`, () => {
 	test('should render correctly', () => {
 		const component = render(<App />, {});
+
+		expect(component.asFragment()).toMatchSnapshot();
+	});
+
+	test('should render correctly when is mobile', () => {
+		Object.defineProperty(reactDeviceDetect, 'isMobile', { get: () => true });
+
+		const component = render(<App />, {});
+
+		const text = component.getByTestId('isMobile');
+		expect(text).toHaveTextContent('This app is for desktop use only');
 
 		expect(component.asFragment()).toMatchSnapshot();
 	});
