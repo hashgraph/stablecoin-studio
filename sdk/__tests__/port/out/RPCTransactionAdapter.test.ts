758569075e79ab5a28f1d38440c3c18f8609a5d8
Adrian Escacena <adrian@io.builders>
2023-09-21 10:12:25 +0200

    add properties

--- sdk/__tests__/port/out/RPCTransactionAdapter.test.ts
+++ sdk/__tests__/port/out/RPCTransactionAdapter.test.ts
@@ -92,7 +92,6 @@ const initSupply = 1000;
 const reserve = 100000000;
 
 describe('🧪 [ADAPTER] RPCTransactionAdapter', () => {
-	
 	let stableCoinCapabilitiesSC: StableCoinCapabilities;
 
 	let th: RPCTransactionAdapter;
