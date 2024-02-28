fbe09550f571747fa1650208461243227fd8cba3
Mario Francia <mario@io.builders>
2024-02-28 13:00:14 +0100

    Removed Hedera Custodians Library dependecies and fixed bugs and tests  (#1050)
    * Removed hedera custodians dependencies and fixed minor bugs and tests
    
    Signed-off-by: Mario Francia <mario@io.builders>
    
    * CR fixes
    
    Signed-off-by: Mario Francia <mario@io.builders>
    
    ---------
    
    Signed-off-by: Mario Francia <mario@io.builders>
    Signed-off-by: Igor Ognichenko <ognichenko.igor@gmail.com>

--- sdk/src/port/out/rpc/RPCTransactionAdapter.ts
+++ sdk/src/port/out/rpc/RPCTransactionAdapter.ts
@@ -1266,7 +1266,10 @@ export default class RPCTransactionAdapter extends TransactionAdapter {
 	 */
 	async connectMetamask(pair = true): Promise<void> {
 		try {
-			const ethProvider = await detectEthereumProvider({ silent: true });
+			const ethProvider = await detectEthereumProvider({
+				mustBeMetaMask: true,
+				silent: true,
+			});
 			if (ethProvider) {
 				this.eventService.emit(WalletEvents.walletFound, {
 					wallet: SupportedWallets.METAMASK,
