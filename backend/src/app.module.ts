0e420c821933fbdcb2249f888f8514e5ab5bf484
Mario Francia <mario@io.builders>
2024-03-04 11:27:43 +0100

    added database and ORM settings
    Signed-off-by: Mario Francia <mario@io.builders>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Specifies that you're using PostgreSQL
+      host: 'localhost', // The address of your database server
+      port: 5432, // The port of your PostgreSQL server
+      username: 'your_username', // Your PostgreSQL username
+      password: 'your_password', // Your PostgreSQL password
+      database: 'your_database', // The name of your database
+      entities: [], // This is where your entities (tables) would go
+      synchronize: true, // In development, it can be true to synchronize the database schema automatically
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
