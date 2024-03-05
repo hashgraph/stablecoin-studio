cd1adb32ab659b6ef4087db42031ddd2f409baba
Miguel_LZPF <miguel.carpena@io.builders>
2024-03-05 15:40:32 +0100

    Update compose configuration and Dockerfile
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -16,7 +16,7 @@ import { Transaction } from './transaction/transaction.entity';
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
+        username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         entities: [Transaction],
