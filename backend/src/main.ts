64a61c60b9518691362b2d7069038c645b2afccb
Mario Francia <mario@io.builders>
2024-03-05 15:05:35 +0100

    fix: dependencies error refactor: .env
    Signed-off-by: Mario Francia <mario@io.builders>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('SERVER_PORT') || 3000;
+  const host = configService.get('SERVER_HOST') || '127.0.0.1';
+
+  await app.listen(port, host);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- backend/src/transaction/transaction.module.ts
+++ backend/src/transaction/transaction.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Transaction } from './transaction.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Transaction])],
   providers: [TransactionService],
-  controllers: [TransactionController]
+  controllers: [TransactionController],
 })
 export class TransactionModule {}
