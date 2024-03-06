d276bebd784e7f20c0306feefbe878cfbed48a5f
Miguel_LZPF <miguel.carpena@io.builders>
2024-03-06 12:23:42 +0100

    Add Swagger documentation to backend API
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
 
   const port = configService.get('SERVER_PORT') || 3000;
   const host = configService.get('SERVER_HOST') || '127.0.0.1';
+
+  const config = new DocumentBuilder()
+    .setTitle('Hedera Multisignature Backend API')
+    .setDescription('The Hedera Multisignature Backend API description')
+    .setVersion('1.0.0')
+    .addTag('hedera')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, host);
   console.log(`Application is running on: ${await app.getUrl()}`);
