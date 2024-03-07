6aede607459ab73530bfd40120190687e6e9e865
Miguel_LZPF <miguel.carpena@io.builders>
2024-03-07 11:20:00 +0100

    Add first functional test and move unit tests to test folder
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- backend/test/transaction/transaction.controller.spec.ts
+++ backend/test/transaction/transaction.controller.spec.ts
@@ -1,7 +1,11 @@
 import { Test } from '@nestjs/testing';
-import TransactionController from 'src/transaction/transaction.controller';
-import { Transaction } from 'src/transaction/transaction.entity';
-import TransactionService from 'src/transaction/transaction.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTransactionRequestDto } from '../../src/transaction/dto/create-transaction-request.dto';
+import TransactionController from '../../src/transaction/transaction.controller';
+import { Transaction } from '../../src/transaction/transaction.entity';
+import TransactionService from '../../src/transaction/transaction.service';
+import { CreateTransactionResponseDto } from '../../src/transaction/dto/create-transaction-response.dto';
 
 describe('Transaction Controller Test', () => {
   let transactionController: TransactionController;
@@ -10,7 +14,13 @@ describe('Transaction Controller Test', () => {
   beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [TransactionController],
-      providers: [TransactionService],
+      providers: [
+        TransactionService,
+        {
+          provide: getRepositoryToken(Transaction),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     transactionService = moduleRef.get<TransactionService>(TransactionService);
@@ -21,26 +31,69 @@ describe('Transaction Controller Test', () => {
 
   describe('Create Transaction', () => {
     it('should create a new transaction', async () => {
-      const result = createMockTransaction();
+      const addTransactionInput = createAddTransactionInput();
+      const createTransactionServiceResult = createMockTransaction();
       jest
         .spyOn(transactionService, 'create')
-        .mockImplementation(() => Promise.resolve(result));
+        .mockImplementation(() =>
+          Promise.resolve(createTransactionServiceResult),
+        );
 
-      expect(await transactionController.addTransaction()).toBe(result);
+      const expectedResult = new CreateTransactionResponseDto(
+        createTransactionServiceResult.id,
+      );
+      const result = await transactionController.addTransaction(addTransactionInput);
+      expect(result.transactionId).toBe(expectedResult.transactionId);
     });
   });
 });
 
-// Example function to create a mock Transaction object
+/**
+ * Creates a sample `CreateTransactionRequestDto` object for testing purposes.
+ * @returns The created `CreateTransactionRequestDto` object.
+ */
+function createAddTransactionInput(): CreateTransactionRequestDto {
+  const createTransactionRequestDto: CreateTransactionRequestDto = {
+    transaction_message:
+      '0a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001803188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001807188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001803188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001809188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001804188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda038801001200',
+    description: 'This transaction is for the creation of a new StableCoin',
+    hedera_account_id: '0.0.123456',
+    key_list: [
+      '75ec8c1997089874ce881690e95900f821a7f69152814728be971e67e4bc2224',
+      '4617e0079f0e943fc407e77ca9fc366f47ccdb4cbec6d5d51eeb996e781c052d',
+      'a0d7a883021253dc9f260ca7934b352f2d75e96d23ebdd1b3851ec0f0f0729d1',
+    ],
+    threshold: 2,
+  };
+
+  return createTransactionRequestDto;
+}
+
+/**
+ * Creates a mock transaction object for testing purposes.
+ * @returns {Transaction} The mock transaction object.
+ */
 function createMockTransaction(): Transaction {
   const transaction = new Transaction();
-  transaction.id = '123e4567-e89b-12d3-a456-426614174000'; // UUID for the transaction
-  transaction.transaction_message = 'Payment for services rendered';
-  transaction.description = 'Monthly web hosting fee';
+  transaction.id = '0.0.2665309'; // ID for the transaction
+  transaction.transaction_message =
+    '0a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001803188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001807188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001803188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001809188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda0388010012000a81012a7f0a7b0a1a0a0b08e8eea5af0610defbe66e12090800100018ddd6a20118001206080010001804188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d055984f43a70e1c62d986a474770611080cab5ee0130ffffffffffffffff7f38ffffffffffffffff7f40004a050880ceda038801001200';
+  transaction.description =
+    'This transaction is for the creation of a new StableCoin';
   transaction.hedera_account_id = '0.0.123456';
-  transaction.signed_messages = ['signature1', 'signature2'];
-  transaction.key_list = ['key1', 'key2'];
-  transaction.signed_keys = ['signedKey1', 'signedKey2'];
+  transaction.signed_messages = [
+    '0ae9012ae6010a7c0a1b0a0c08f6eca5af0610ea98a1ad0112090800100018ddd6a20118001206080010001806188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d',
+    '0ae9012ae6010a7c0a1b0a0c08f6eca5af0610ea98a1ad0112090800100018ddd6a20118001206080010001806188084af5f2202087832005a4a0a22122094ac3f274e59cb947c4685d16cfa2d8a5d',
+  ];
+  transaction.key_list = [
+    '75ec8c1997089874ce881690e95900f821a7f69152814728be971e67e4bc2224',
+    '4617e0079f0e943fc407e77ca9fc366f47ccdb4cbec6d5d51eeb996e781c052d',
+    'a0d7a883021253dc9f260ca7934b352f2d75e96d23ebdd1b3851ec0f0f0729d1',
+  ];
+  transaction.signed_keys = [
+    '75ec8c1997089874ce881690e95900f821a7f69152814728be971e67e4bc2224',
+    '4617e0079f0e943fc407e77ca9fc366f47ccdb4cbec6d5d51eeb996e781c052d',
+  ];
   transaction.status = 'SIGNED'; // or 'PENDING' depending on the test case
   transaction.threshold = 2;
 
