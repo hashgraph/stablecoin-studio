53f1355c188843ea734ba09fd9e5257eaeec61dc
Miguel_LZPF <miguel.carpena@io.builders>
2024-04-05 14:20:03 +0200

    docs: [cli] add automatic license header to files
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- cli/src/resources/config/language.ts
+++ cli/src/resources/config/language.ts
@@ -1,6 +1,26 @@
-import colors from 'colors';
+/*
+ *
+ * Hedera Stablecoin CLI
+ *
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import { AccountType } from '../../domain/configuration/interfaces/AccountType';
 import * as inquirer from 'inquirer';
+import colors from 'colors';
 
 const separator_1 = {
   Separator_1: new inquirer.Separator(),
--- cli/src/resources/license-header.js
+++ cli/src/resources/license-header.js
@@ -17,17 +17,3 @@
  * limitations under the License.
  *
  */
-
-import { RequestPublicKey } from '@hashgraph/stablecoin-npm-sdk';
-
-export interface IManagedFeatures {
-  name?: string;
-  symbol?: string;
-  adminKey?: RequestPublicKey;
-  supplyKey?: RequestPublicKey;
-  freezeKey?: RequestPublicKey;
-  wipeKey?: RequestPublicKey;
-  pauseKey?: RequestPublicKey;
-  kycKey?: RequestPublicKey;
-  feeScheduleKey?: RequestPublicKey;
-}
