8384316ec05ed779c831cf1097c29ea6c792a2bb
Mario Francia <mario@io.builders>
2024-05-14 09:18:21 +0200

    hotfix: start date multisig (#1123)
    * fix: startdate
    
    Signed-off-by: Mario Francia <mario@io.builders>
    
    * fix: startdate
    
    Signed-off-by: Mario Francia <mario@io.builders>
    
    ---------
    
    Signed-off-by: Mario Francia <mario@io.builders>

--- sdk/src/port/out/hs/HederaTransactionAdapter.ts
+++ sdk/src/port/out/hs/HederaTransactionAdapter.ts
@@ -1402,15 +1402,11 @@ export abstract class HederaTransactionAdapter extends TransactionAdapter {
 	abstract getAccount(): Account;
 
 	abstract signAndSendTransaction(
-		...args:
-			| [t: Transaction, startDate?: string]
-			| [
-					t: Transaction,
-					transactionType: TransactionType,
-					nameFunction?: string,
-					abi?: any[],
-					startDate?: string,
-			  ]
+		t: Transaction,
+		transactionType: TransactionType,
+		nameFunction?: string,
+		abi?: any[],
+		startDate?: string,
 	): Promise<TransactionResponse>;
 
 	abstract sign(message: string | Transaction): Promise<string>;
