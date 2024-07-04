a6f795a55917a3d8fced6816ec6910297631237c
Mario Francia <mario@io.builders>
2024-07-04 11:58:49 +0200

    refactor: web test
    Signed-off-by: Mario Francia <mario@io.builders>

--- web/src/views/MultiSigTransactions/__test__/MultisigTransactions.test.tsx
+++ web/src/views/MultiSigTransactions/__test__/MultisigTransactions.test.tsx
@@ -1,29 +1,17 @@
 import MultiSigTransactions from '../MultisigTransactions';
-import { render } from '../../../test/index';
-import { Provider } from 'react-redux';
-import { ChakraProvider } from '@chakra-ui/react';
-import store from '../../../store/store';
+import {render} from '../../../test';
 import translations from '../../../translations/en/multiSig.json';
 
 describe(`<${MultiSigTransactions.name} />`, () => {
-	beforeEach(() => {});
-
-	const renderWithProviders = (ui: any) => {
-		return render(
-			<Provider store={store}>
-				<ChakraProvider>{ui}</ChakraProvider>
-			</Provider>,
-		);
-	};
 
 	test('should render correctly', () => {
-		const component = renderWithProviders(<MultiSigTransactions />);
+		const component = render(<MultiSigTransactions />);
 
 		expect(component.asFragment()).toMatchSnapshot();
 	});
 
 	test('should have title', () => {
-		const component = renderWithProviders(<MultiSigTransactions />);
+		const component = render(<MultiSigTransactions />);
 		const header = component.getByTestId('base-container-heading');
 
 		expect(header).toHaveTextContent(translations.title);
