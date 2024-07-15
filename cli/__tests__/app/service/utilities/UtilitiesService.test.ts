ed3559d3e4fecf5340fac2c8baed561d1893b002
Alberto Molina <alberto@io.builders>
2024-07-15 17:40:54 +0200

    feat: utilities tests added
    Signed-off-by: Alberto Molina <alberto@io.builders>

--- cli/__tests__/app/service/utilities/UtilitiesService.test.ts
+++ cli/__tests__/app/service/utilities/UtilitiesService.test.ts
@@ -19,91 +19,175 @@
  */
 
 import { Network } from '@hashgraph/stablecoin-npm-sdk';
-import { utilsService, configurationService } from '../../../../src/index.js';
+import {
+  utilsService,
+  configurationService,
+  setMirrorNodeService,
+  setRPCService,
+} from '../../../../src/index.js';
 import { AccountType } from '../../../../src/domain/configuration/interfaces/AccountType';
+import { MIRROR_NODE, RPC } from '../../../../src/core/Constants.js';
 
 describe('UtilitiesService', () => {
-  it('should initialize the SDK and connect to the network', async () => {
-    // mocks
-    const mockAccount = {
-      accountId: 'mockAccountId',
-      type: AccountType.SelfCustodial,
-      selfCustodial: {
-        privateKey: {
-          key: 'mockPrivateKey',
-          type: 'mockPrivateKeyType',
-        },
+  const network = 'testnet';
+  // mocks
+  const mockAccount = {
+    accountId: 'mockAccountId',
+    type: AccountType.SelfCustodial,
+    selfCustodial: {
+      privateKey: {
+        key: 'mockPrivateKey',
+        type: 'mockPrivateKeyType',
       },
-      network: 'mockNetwork',
-      alias: 'mockAlias',
-    };
+    },
+    network: network,
+    alias: 'mockAlias',
+  };
 
-    const mockCurrentNetwork = {
-      name: 'mockNetworkName',
-    };
+  const mockCurrentNetwork = {
+    name: network,
+    chainId: 1234,
+    consensusNodes: [
+      {
+        url: 'whatever',
+        nodeId: '3',
+      },
+    ],
+  };
 
-    const mockCurrentMirror = {
-      name: 'testnet',
-      network: 'testnet',
-      baseUrl: 'https://testnet.mirrornode.com',
-      apiKey: '',
-      headerName: '',
-      selected: true,
-    };
-    const mockCurrentRPC = {
-      name: 'testnet',
-      network: 'testnet',
-      baseUrl: 'https://testnet.rpc.com',
-      apiKey: '',
-      headerName: '',
-      selected: true,
-    };
+  const mockCurrentMirror = {
+    name: 'testnet',
+    network: network,
+    baseUrl: 'https://testnet.mirrornode.com',
+    apiKey: '',
+    headerName: '',
+    selected: true,
+  };
+  const mockCurrentRPC = {
+    name: 'testnet',
+    network: network,
+    baseUrl: 'https://testnet.rpc.com',
+    apiKey: '',
+    headerName: '',
+    selected: true,
+  };
 
-    const mockCurrentBackend = {
-      endpoint: 'http://localhost:3000/path',
-    };
+  const mockCurrentBackend = {
+    endpoint: 'http://localhost:3000/path',
+  };
 
-    const mockLogConfiguration = {
-      level: 'debug',
-    };
+  const mockLogConfiguration = {
+    level: 'debug',
+  };
 
-    const networkInitSpy = jest
-      .spyOn(Network, 'init')
-      .mockResolvedValue(undefined);
-    const networkConnectSpy = jest
+  const mockCurrentFactory = {
+    id: '0.0.1234567',
+    network: network,
+  };
+
+  const mockCurrentHederaTokenManager = {
+    id: '0.0.7654321',
+    network: network,
+  };
+
+  configurationService.getLogConfiguration = jest
+    .fn()
+    .mockReturnValue(mockLogConfiguration);
+
+  configurationService.getConfiguration = jest.fn().mockReturnValue({
+    defaultNetwork: 'testnet',
+    networks: [mockCurrentNetwork],
+    accounts: [mockAccount],
+    mirrors: [mockCurrentMirror],
+    rpcs: [mockCurrentRPC],
+    backend: mockCurrentBackend,
+    logs: mockLogConfiguration,
+    factories: [mockCurrentFactory],
+  });
+
+  let networkInitSpy;
+  let networkConnectSpy;
+  beforeEach(() => {
+    networkInitSpy = jest.spyOn(Network, 'init').mockResolvedValue(undefined);
+    networkConnectSpy = jest
       .spyOn(Network, 'connect')
       .mockResolvedValue(undefined);
+  });
 
-    configurationService.getLogConfiguration = jest
-      .fn()
-      .mockReturnValue(mockLogConfiguration);
-    utilsService.getCurrentAccount = jest.fn().mockReturnValue(mockAccount);
-    utilsService.getCurrentNetwork = jest
-      .fn()
-      .mockReturnValue(mockCurrentNetwork);
-    utilsService.getCurrentMirror = jest
-      .fn()
-      .mockReturnValue(mockCurrentMirror);
-    utilsService.getCurrentRPC = jest.fn().mockReturnValue(mockCurrentRPC);
-    utilsService.getCurrentBackend = jest
-      .fn()
-      .mockReturnValue(mockCurrentBackend);
-
-    // method call
-    await utilsService.initSDK();
-
-    // verify
-    expect(configurationService.getLogConfiguration).toHaveBeenCalledTimes(1);
-    expect(utilsService.getCurrentAccount).toHaveBeenCalledTimes(1);
-    expect(utilsService.getCurrentNetwork).toHaveBeenCalledTimes(1);
-    expect(utilsService.getCurrentMirror).toHaveBeenCalledTimes(1);
-    expect(utilsService.getCurrentRPC).toHaveBeenCalledTimes(1);
-    expect(utilsService.getCurrentBackend).toHaveBeenCalledTimes(1);
-    expect(networkInitSpy).toHaveBeenCalledTimes(1);
-    expect(networkConnectSpy).toHaveBeenCalledTimes(1);
-
+  afterEach(() => {
     // restore mocks
     networkInitSpy.mockRestore();
     networkConnectSpy.mockRestore();
+  });
+
+  it('should initialize the SDK and connect to the network', async () => {
+    // method call
+    await expect(utilsService.initSDK()).rejects.toThrow();
+    await utilsService.setCurrentAccount(mockAccount);
+    await expect(utilsService.initSDK()).rejects.toThrow();
+    await utilsService.setCurrentNetwotk(mockCurrentNetwork);
+    await expect(utilsService.initSDK()).rejects.toThrow();
+    await utilsService.setCurrentMirror(mockCurrentMirror);
+    await expect(utilsService.initSDK()).rejects.toThrow();
+    await utilsService.setCurrentBackend(mockCurrentBackend);
+    await expect(utilsService.initSDK()).rejects.toThrow();
+    await utilsService.setCurrentRPC(mockCurrentRPC);
+    await utilsService.setCurrentFactory(mockCurrentFactory);
+    await utilsService.setCurrentHederaTokenManager(
+      mockCurrentHederaTokenManager,
+    );
+    await utilsService.initSDK();
+
+    const account = utilsService.getCurrentAccount();
+    const backend = utilsService.getCurrentBackend();
+    const factory = utilsService.getCurrentFactory();
+    const hederaTokenManager = utilsService.getCurrentHederaTokenManager();
+    const mirrorNode = utilsService.getCurrentMirror();
+    const network = utilsService.getCurrentNetwork();
+    const rpc = utilsService.getCurrentRPC();
+
+    // verify
+    expect(configurationService.getLogConfiguration).toHaveBeenCalledTimes(5);
+    expect(account).toEqual(mockAccount);
+    expect(backend).toEqual(mockCurrentBackend);
+    expect(factory).toEqual(mockCurrentFactory);
+    expect(hederaTokenManager).toEqual(mockCurrentHederaTokenManager);
+    expect(mirrorNode).toEqual(mockCurrentMirror);
+    expect(network).toEqual(mockCurrentNetwork);
+    expect(rpc).toEqual(mockCurrentRPC);
+  });
+
+  it('configure network', async () => {
+    jest
+      .spyOn(setMirrorNodeService, 'manageMirrorNodeMenu')
+      .mockImplementation(async (_network: string): Promise<void> => {
+        expect(_network).toEqual(network);
+      });
+    jest
+      .spyOn(setRPCService, 'manageRPCMenu')
+      .mockImplementation(async (_network: string): Promise<void> => {
+        expect(_network).toEqual(network);
+      });
+
+    jest.spyOn(utilsService, 'showError').mockImplementation();
+
+    // method call
+    await utilsService.setCurrentAccount(mockAccount);
+    await utilsService.setCurrentNetwotk(mockCurrentNetwork);
+    await utilsService.setCurrentMirror(mockCurrentMirror);
+    await utilsService.setCurrentBackend(mockCurrentBackend);
+    await utilsService.setCurrentRPC(mockCurrentRPC);
+    await utilsService.setCurrentFactory(mockCurrentFactory);
+    await utilsService.setCurrentHederaTokenManager(
+      mockCurrentHederaTokenManager,
+    );
+    await utilsService.initSDK();
+
+    jest.spyOn(utilsService, 'defaultMultipleAsk').mockResolvedValue(network);
+
+    await utilsService.configureNetwork(MIRROR_NODE);
+    await utilsService.configureNetwork(RPC);
+    await utilsService.configureNetwork('nothing');
+    expect(utilsService.showError).toHaveBeenCalledTimes(1);
   });
 });
