a4f0361e8be0d61a12c341eb4c0477edc3814788
Mario Francia <mario@io.builders>
2024-09-30 11:17:01 +0200

    refactor: removed hashconnect dependencies
    Signed-off-by: Mario Francia <mario@io.builders>

--- changeProyectsReferencesToRepo.sh
+++ changeProyectsReferencesToRepo.sh
@@ -7,15 +7,12 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   # macOS
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
 elif [[ "$OSTYPE" == "msys" ]]; then
   # Windows
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
   find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
 fi
