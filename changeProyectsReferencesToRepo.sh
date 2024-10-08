616321bce5dccdc10811a1a139e97e86a0f95877
Miguel_LZPF <miguel.carpena@io.builders>
2024-10-08 06:53:02 +0200

    [BBND-303] GitHub Actions Redesign (#1190)
    * fix: update .gitignore files for improved environment and dependency management
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * fix: improved "all in one" old test
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * feat: add reusable workflows for building and testing CLI, SDK, Contracts, and Web
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * feat: add GitHub workflows for testing CLI and Web applications
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * feat: replace changePomVersion.sh with enhanced changeVersion.sh for version updates
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * fix: rename cleanCache scripts to clear-cache for consistency across workflows
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * feat: remove outdated build workflows for contracts, CLI, SDK, and web applications
    fix: update test workflows for backend and web with improved caching and dependency management
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * fix: comment out build step in web testing workflow
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * fix: remove test folder
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * chore: test action
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * fix: update initial steps action to improve dependency installation options
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: streamline initial steps in backend, CLI, web, contracts, and SDK workflows
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: consolidate installation and build steps for Contracts, SDK, CLI, and Web workflows
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: update workflow configurations for publishing
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: update workflow paths for backend, CLI, contracts, SDK, and web testing
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: update job dependencies in publish workflow for improved execution order
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: simplify version update process in changeProyectsReferencesToRepo.sh
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: enhance initial steps action with new dependency options for contracts and env file
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: publish separated in jobs
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * refactor: update workflow to add version check workflow
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * chore: fix changeVersion and upload final workflows
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * chore: trigger test
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    * chore: remove trigger
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>
    
    ---------
    
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- changeProyectsReferencesToRepo.sh
+++ changeProyectsReferencesToRepo.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 # example: ./changeVersionToPublish.sh 1.0.0
 
+# Get the version from package.json
 version=$(node -p -e "require('./package.json').version")
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOS
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i '' "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  # Linux
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
-elif [[ "$OSTYPE" == "msys" ]]; then
-  # Windows
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-sdk\": \".*\"/\"@hashgraph\/stablecoin-npm-sdk\": \"$version\"/g"
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/stablecoin-npm-contracts\": \".*\"/\"@hashgraph\/stablecoin-npm-contracts\": \"$version\"/g" 
-  find . -path ./node_modules -prune -o -name "package.json" -type f  | grep -v node_modules | grep -v example | xargs sed -i "s/\"@hashgraph\/hashconnect\": \".*\"/\"@hashgraph\/hashconnect\": \"$version\"/g"
-fi
+# Define an array of package names
+packages=(
+  "@hashgraph/stablecoin-npm-sdk"
+  "@hashgraph/stablecoin-npm-contracts"
+  "@hashgraph/hashconnect"
+)
+
+# Function to update the version in package.json files
+update_version() {
+  local package=$1
+  local version=$2
+
+  # Find all package.json files, excluding node_modules and example directories
+  find . -path ./node_modules -prune -o -name "package.json" -type f | grep -v node_modules | grep -v example | while read -r file; do
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      # For macOS, use the correct sed syntax with no backup file
+      sed -i '' "s|\"$package\": \".*\"|\"$package\": \"$version\"|g" "$file"
+    else
+      # For Linux/Windows, use the default sed -i syntax
+      sed -i "s|\"$package\": \".*\"|\"$package\": \"$version\"|g" "$file"
+    fi
+  done
+}
+
+# Update the version for each package
+for package in "${packages[@]}"; do
+  update_version "$package" "$version"
+done
