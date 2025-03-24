6ac8d00c84daffb8b437b9cd23ea0e37b7dd3501
Miguel_LZPF <miguel.carpena@io.builders>
2025-03-24 15:37:38 +0100

    refactor: remove unused mock deployment function from tests
    Signed-off-by: Miguel_LZPF <miguel.carpena@io.builders>

--- contracts/test/thread0/supplieradmin.test.ts
+++ contracts/test/thread0/supplieradmin.test.ts
@@ -21,9 +21,7 @@ describe('➡️ Supplier Admin Tests', function () {
         // * Deploy StableCoin Token
         console.info(MESSAGES.deploy.info.deployFullInfrastructureInTests)
         ;[operator, nonOperator] = await ethers.getSigners()
-        // if ((network.name as NetworkName) === NETWORK_LIST.name[0]) {
-        //     await deployPrecompiledHederaTokenServiceMock(hre, signer)
-        // }
+        // * Deploy contracts
         ;({ proxyAddress } = await deployFullInfrastructureInTests({
             signer: operator,
             network: network.name as NetworkName,
@@ -300,9 +298,7 @@ describe('➡️ Supplier Admin Tests - (Unlimited)', function () {
         // * Deploy StableCoin Token
         console.info(MESSAGES.deploy.info.deployFullInfrastructureInTests)
         ;[operator, nonOperator] = await ethers.getSigners()
-        // if ((network.name as NetworkName) === NETWORK_LIST.name[0]) {
-        //     await deployPrecompiledHederaTokenServiceMock(hre, signer)
-        // }
+        // * Deploy contracts
         ;({ proxyAddress, tokenAddress } = await deployFullInfrastructureInTests({
             signer: operator,
             network: network.name as NetworkName,
