bdb1b8e7dfeeae7dd0dea46f27aeb0108c795a8f
Mario Francia <mario@io.builders>
2025-04-23 12:29:19 +0200

    `feat(constants): add storage positions for burnable, pausable, kyc, and other modules`
    Signed-off-by: Mario Francia <mario@io.builders>

--- contracts/contracts/constants/storagePositions.sol
+++ contracts/contracts/constants/storagePositions.sol
@@ -27,4 +27,35 @@ bytes32 constant _RESERVE_STORAGE_POSITION = 0x6cd87a21205e289ae515ee48016344e9a
 // keccak256('stablecoin.studio.hold.storage');
 bytes32 constant _HOLD_STORAGE_POSITION = 0x55eb875db6f21625db342149ef0f0b78f6afec1625e2c812812e031b05ee4bd1;
 
+// keccak256('stablecoin.studio.burnable.storage');
+bytes32 constant _BURNABLE_STORAGE_POSITION = 0xfda2cae676ea43421a1c27cf154ea61be52a0bdc364f4fe0308ca3dc3b386d88;
+
+// keccak256('stablecoin.studio.cash.in.storage');
+bytes32 constant _CASH_IN_STORAGE_POSITION = 0x2f36d6652956f8a3abaf21f711b674bc479d3e0b028c033eae96f3c09bfbfcf4;
+
+// keccak256('stablecoin.studio.custom.fees.storage');
+bytes32 constant _CUSTOM_FEES_STORAGE_POSITION = 0xfe7faefcc90fbda0a6480fc49dc41816d2f8c851687a54052ad56dbd37d06800;
+
+// keccak256('stablecoin.studio.deletable.storage');
+bytes32 constant _DELETABLE_STORAGE_POSITION = 0x40dff76c064efe1e221cbd1acf85ed34ba6aa87e092b104e2f7c5165204a3b15;
+
+// keccak256('stablecoin.studio.pausable.storage');
+bytes32 constant _PAUSABLE_STORAGE_POSITION = 0xdded967955d63b2cb16d6d5775f7267e43f7086cacf3ce210808d3843411def4;
+
+// keccak256('stablecoin.studio.rescuable.storage');
+bytes32 constant _RESCUABLE_STORAGE_POSITION = 0x31459db608f5ae129df7dd7995a9896b75f0414983b1c1e48af077add11dc2e5;
+
+// keccak256('stablecoin.studio.role.management.storage');
+bytes32 constant _ROLE_MANAGEMENT_STORAGE_POSITION = 0xf76630ecf6859cc87678d616f74e9b31e8d6cb471327a3a36db12416545b09ad;
+
+// keccak256('stablecoin.studio.wipeable.storage');
+bytes32 constant _WIPEABLE_STORAGE_POSITION = 0xf481fafecb55bf3d4602fe346643ea4624031d4094be71f3b5cf069cebe1359e;
+
+// keccak256('stablecoin.studio.freezable.storage');
+bytes32 constant _FREEZABLE_STORAGE_POSITION = 0x1003b36e47ed2637184fc728fa9b5aa22ee1f182f4ef6ba9bff9b94ec8b54a15;
+
+// keccak256('stablecoin.studio.kyc.storage');
+bytes32 constant _KYC_STORAGE_POSITION = 0x8c9912ca4c42f41d10cec25d6001cc394cff894c6d66c9267dc9f7d8eb9ce6a1;
+
+
 // solhint-enable max-line-length
